a0bd4e2a40010a94ac8694318401e57f86701962
Richard Iannone <riannone@me.com>
2017-02-26 20:59:25 -0800

    Add 'get_summary' function

---
+++ R/get_summary.R
@@ -0,0 +1,16 @@
+#' Create a simple summary of the
+#' interrogation
+#' @description Gets the essential information
+#' from an agent object after an interrogation
+#' is complete
+#' @return an agent object.
+#' @export get_summary
+
+get_summary <- function(agent) {
+  
+  if (all(agent$validation_set$all_passed %in% c(TRUE, FALSE))) {
+    return(agent$validation_set[, c(1:11)])
+  } else {
+    stop("An interrogation hasn't yet occurred.")
+  }
+}

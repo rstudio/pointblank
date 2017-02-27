d3358d94f716f29ce000a10d4e674fbd90d534c0
Richard Iannone <riannone@me.com>
2017-02-26 20:35:12 -0800

    Allow for a regex interrogation

--- R/interrogate.R
+++ R/interrogate.R
@@ -70,8 +70,7 @@ interrogate <- function(agent) {
     if (agent$validation_set$assertion_type[i] %in%
         c("col_vals_in_set")) {
       
-      # Get operator values for all assertion types involving
-      # simple operator comparisons
+      # Get the set values for the expression
       if (agent$validation_set$assertion_type[i] == "col_vals_in_set") {
         set <- agent$validation_set$set[[i]][[1]][[1]]
       }
@@ -88,6 +87,25 @@ interrogate <- function(agent) {
            "pb_is_good_"))
     }
     
+    if (agent$validation_set$assertion_type[i] %in%
+        c("col_vals_regex")) {
+      
+      # Get the regex matching statement
+      if (agent$validation_set$assertion_type[i] == "col_vals_regex") {
+        regex <- agent$validation_set$regex[i]
+      }
+      
+      # Get the final judgment on the table and the query
+      judgment <- 
+        table %>%
+        dplyr::mutate_(.dots = setNames(
+          paste0("grepl(\"",
+            agent$validation_set$regex[i],
+            "\", ", agent$validation_set$column[i],
+            ")"),
+          "pb_is_good_"))
+    }
+    
     # Get total count of rows
     n <-
       judgment %>%
@@ -96,7 +114,29 @@ interrogate <- function(agent) {
       tibble::as_tibble() %>%
       .$row_count
     
+    # Get total count of TRUE rows
+    n_passed <-
+      judgment %>%
+      filter(pb_is_good_ == TRUE) %>%
+      dplyr::group_by() %>%
+      dplyr::summarize(row_count = n()) %>%
+      tibble::as_tibble() %>%
+      .$row_count
+    
+    # Get total count of FALSE rows
+    n_failed <-
+      judgment %>%
+      filter(pb_is_good_ == FALSE) %>%
+      dplyr::group_by() %>%
+      dplyr::summarize(row_count = n()) %>%
+      tibble::as_tibble() %>%
+      .$row_count
+    
     agent$validation_set$n[i] <- n
+    agent$validation_set$n_passed[i] <- n_passed
+    agent$validation_set$n_failed[i] <- n_failed
+    agent$validation_set$f_passed[i] <- round((n_passed / n), 3)
+    agent$validation_set$f_failed[i] <- round((n_failed / n), 3)
     
     # Get count of rows where `pb_is_good_ == FALSE`
     false_count <-
@@ -109,8 +149,10 @@ interrogate <- function(agent) {
     
     if (false_count > 0) {
       
+      # State that `all_passed` is FALSE
       agent$validation_set$all_passed[i] <- FALSE
       
+      # Collect up to 5 problem rows 
       problem_rows <- 
         judgment %>%
         dplyr::filter(pb_is_good_ == FALSE) %>%
@@ -118,10 +160,14 @@ interrogate <- function(agent) {
         head(5) %>%
         tibble::as_tibble()
       
+      # Place the sample of problem rows in
+      # the `agent$validation_set` tbl_df
+      # as a nested tbl_df
       agent$validation_set$row_sample[i] <- 
         problem_rows %>%
-        tidyr::nest_(key_col = "data", nest_cols = names(.))
-      
+        tidyr::nest_(
+          key_col = "data",
+          nest_cols = names(.))
       
     } else if (false_count == 0) {
       agent$validation_set$all_passed[i] <- TRUE

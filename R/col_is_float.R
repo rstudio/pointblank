0da3ae5058f7fa35441d88deda81000af0dd91e6
Richard Iannone <riannone@me.com>
2017-03-02 19:00:59 -0800

    Add function 'col_is_float'

---
+++ R/col_is_float.R
@@ -0,0 +1,43 @@
+#' Verify that a column contains only floating
+#' point values
+#' @description Set a verification step where
+#' a table column is expected to consist of
+#' floating point values.
+#' @return an agent object.
+#' @importFrom tibble tibble
+#' @importFrom dplyr bind_rows
+#' @export col_is_float
+
+col_is_float <- function(agent,
+                         column,
+                         report_count = 0,
+                         warn_count = 1,
+                         notify_count = 2,
+                         tbl_name = NULL,
+                         db_type = NULL,
+                         creds_file = NULL,
+                         initial_sql = NULL) {
+  
+  assertion_type <- "col_is_float"
+  
+  validation_step <-
+    create_validation_step(
+      agent = agent,
+      assertion_type = assertion_type,
+      column = column,
+      report_count = report_count,
+      warn_count = warn_count,
+      notify_count = notify_count,
+      tbl_name = ifelse(is.null(tbl_name), as.character(NA), tbl_name),
+      db_type = ifelse(is.null(db_type), as.character(NA), db_type),
+      creds_file = ifelse(is.null(creds_file), as.character(NA), creds_file),
+      init_sql = ifelse(is.null(initial_sql), as.character(NA), initial_sql))
+  
+  # Append `validation_component` to `validation_set`
+  agent$validation_set <-
+    dplyr::bind_rows(
+      agent$validation_set,
+      validation_step)
+  
+  return(agent)
+}

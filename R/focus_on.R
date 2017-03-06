fe94490bcd15e4b571fba815a0e4307a18840c0a
Richard Iannone <riannone@me.com>
2017-03-06 00:44:26 -0800

    Store the column names for a tbl in focus

--- R/focus_on.R
+++ R/focus_on.R
@@ -3,6 +3,8 @@
 #' focus on the table name, database type, and
 #' the location of the credentials file. 
 #' @return an agent object.
+#' @importFrom dplyr filter
+#' @importFrom tibble as_tibble
 #' @export focus_on
 
 focus_on <- function(agent,
@@ -30,6 +32,32 @@ focus_on <- function(agent,
   } else if (!is.null(initial_sql)) {
     agent$focal_init_sql <- initial_sql
   }
+  
+  # Make the table accessible to obtain basic
+  # information from it
+  
+  if (agent$focal_db_type == "local") {
+    
+    # Create `table` object as the direct reference to a
+    # local `data.frame` or `tbl_df` object
+    table <- get(tbl_name)
+    
+  } else if (agent$focal_db_type == "PostgreSQL") {
+    
+    # Create `table` object as an SQL entry point for a remote table
+    table <- 
+      set_entry_point(
+        table = tbl_name,
+        db_type = db_type,
+        creds_file = creds_file)
+  }
+  
+  # Get the column names from the table
+  agent$focal_col_names <-
+    table %>%
+    dplyr::filter(row_number() == 1) %>%
+    tibble::as_tibble() %>%
+    names()
   
   return(agent)
 }

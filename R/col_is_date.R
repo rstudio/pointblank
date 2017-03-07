68d06498a6a59488434a813782ed2259592eaf9a
Richard Iannone <riannone@me.com>
2017-03-06 22:32:36 -0800

    Add function 'col_is_date'

---
+++ R/col_is_date.R
@@ -0,0 +1,48 @@
+#' Verify that a column contains only R Date objects
+#' @description Set a verification step where
+#' a table column is expected to consist entirely
+#' of R \code{Date} objects.
+#' @return an agent object.
+#' @importFrom tibble tibble
+#' @importFrom dplyr bind_rows
+#' @export col_is_date
+
+col_is_date <- function(agent,
+                        column,
+                        report_count = 0,
+                        warn_count = 1,
+                        notify_count = 1,
+                        tbl_name = NULL,
+                        db_type = NULL,
+                        creds_file = NULL,
+                        initial_sql = NULL) {
+  
+  assertion_type <- "col_is_date"
+  
+  # If "*" is provided for `column`, select all
+  # table columns for this verification
+  if (column[1] == "*") {
+    column <- get_all_cols(agent = agent)
+  }
+  
+  validation_step <-
+    create_validation_step(
+      agent = agent,
+      assertion_type = assertion_type,
+      column = column,
+      report_count = report_count,
+      warn_count = warn_count,
+      notify_count = notify_count,
+      tbl_name = ifelse(is.null(tbl_name), as.character(NA), tbl_name),
+      db_type = ifelse(is.null(db_type), as.character(NA), db_type),
+      creds_file = ifelse(is.null(creds_file), as.character(NA), creds_file),
+      init_sql = ifelse(is.null(initial_sql), as.character(NA), initial_sql))
+  
+  # Append `validation_component` to `validation_set`
+  agent$validation_set <-
+    dplyr::bind_rows(
+      agent$validation_set,
+      validation_step)
+  
+  return(agent)
+}

348dbf4d7144d1b5f96aa282c02e5f9a7ba3526a
Richard Iannone <riannone@me.com>
2017-03-10 23:18:53 -0800

    Add magrittr pipe to package

---
+++ R/pipe.R
@@ -0,0 +1,10 @@
+#' The magrittr pipe
+#'
+#' @description pointblank uses the pipe function,
+#' \code{\%>\%} to turn function composition into a
+#' series of imperative statements.
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @rdname pipe
+#' @export
+NULL

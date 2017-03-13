2e1eadc51de60acc3e349b7e9969818f449ec338
Richard Iannone <riannone@me.com>
2017-03-13 14:28:53 -0700

    Add tests for `get_summary()` function

---
+++ tests/testthat/test-get_summary.R
@@ -0,0 +1,50 @@
+context("Creating a validation summary")
+
+test_that("Getting a validation summary is possible", {
+  
+  library(tibble)
+  library(dplyr)
+  
+  # Use `col_is_character()` function to create
+  # a validation step
+  validation <-
+    create_agent() %>%
+    focus_on(
+      file_name = system.file("extdata", "small_table.csv", package = "pointblank")) %>%
+    col_is_character(column = "b") %>%
+    interrogate()
+  
+  # Get a summary of the validation
+  summary <- get_summary(validation)
+  
+  # Expect that the summary has specific column names
+  expect_equivalent(
+    colnames(summary),
+    c("tbl_name", "db_type", "assertion_type", "column", "value", "set",
+      "regex", "preconditions", "all_passed", "n", "n_passed"))
+  
+  # Expect a single row in this summary
+  expect_equivalent(nrow(summary), 1)
+  
+  # Expect certain column types for this summary
+  expect_is(summary$tbl_name, "character")
+  expect_is(summary$db_type, "character")
+  expect_is(summary$assertion_type, "character")
+  expect_is(summary$column, "character")
+  expect_is(summary$value, "numeric")
+  expect_is(summary$set, "list")
+  expect_is(summary$regex, "character")
+  expect_is(summary$preconditions, "list")
+  expect_is(summary$all_passed, "logical")
+  expect_is(summary$n, "numeric")
+  expect_is(summary$n_passed, "numeric")
+  
+  # Expect an error if getting a summary without an interrogation
+  expect_error(
+    create_agent() %>%
+    focus_on(
+      file_name = system.file("extdata", "small_table.csv", package = "pointblank")) %>%
+    col_is_character(column = "b") %>%
+    get_summary())
+  
+})

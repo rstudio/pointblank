cc06fe4b16fd4194e297f10c5d8362b5a46cfa87
Richard Iannone <riannone@me.com>
2017-03-14 09:25:58 -0700

    Add test for `rows_not_duplicated()` fcn

--- tests/testthat/test-create_validation_steps.R
+++ tests/testthat/test-create_validation_steps.R
@@ -1043,3 +1043,56 @@ test_that("Creating a `col_vals_not_null()` step is possible", {
                     system.file("extdata", "small_table.csv",
                                 package = "pointblank"))
 })
+
+test_that("Creating a `rows_not_duplicated()` step is possible", {
+  
+  library(tibble)
+  library(dplyr)
+  
+  # Use `col_is_posix()` function to create
+  # a validation step
+  validation <-
+    create_agent() %>%
+    focus_on(
+      file_name = system.file("extdata", "small_table.csv", package = "pointblank")) %>%
+    rows_not_duplicated()
+  
+  # Expect the class name for the object
+  # to be `ptblank_agent`
+  expect_is(validation, "ptblank_agent")
+  
+  # Expect elements of the object to be equivalent
+  # to specific parameters
+  expect_equivalent(validation$focal_tbl_name, "small_table")
+  expect_equivalent(validation$focal_db_type, "local_file")
+  expect_equivalent(validation$focal_col_names, c("date_time", "a", "b", "c", "d"))
+  expect_true(is.na(validation$focal_db_cred_file_path))
+  expect_true(is.na(validation$focal_init_sql))
+  expect_equivalent(validation$validation_set$tbl_name, "small_table")
+  expect_equivalent(validation$validation_set$db_type, "local_file")
+  expect_equivalent(validation$validation_set$assertion_type, "rows_not_duplicated")
+  expect_true(is.na(validation$validation_set$column))
+  expect_true(is.na(validation$validation_set$value))
+  expect_null(validation$validation_set$set[[1]])
+  expect_true(is.na(validation$validation_set$regex))
+  expect_null(validation$validation_set$preconditions[[1]])
+  expect_true(is.na(validation$validation_set$all_passed))
+  expect_true(is.na(validation$validation_set$n))
+  expect_true(is.na(validation$validation_set$n_passed))
+  expect_true(is.na(validation$validation_set$n_failed))
+  expect_true(is.na(validation$validation_set$f_passed))
+  expect_true(is.na(validation$validation_set$f_failed))
+  expect_equivalent(validation$validation_set$report_count, 0)
+  expect_equivalent(validation$validation_set$warn_count, 1)
+  expect_equivalent(validation$validation_set$notify_count, 2)
+  expect_true(is.na(validation$validation_set$report))
+  expect_true(is.na(validation$validation_set$warn))
+  expect_true(is.na(validation$validation_set$notify))
+  expect_true(is.na(validation$validation_set$row_sample))
+  expect_true(is.na(validation$validation_set$init_sql))
+  expect_true(is.na(validation$validation_set$db_cred_file_path))
+  expect_equivalent(validation$validation_set$file_path,
+                    system.file("extdata", "small_table.csv",
+                                package = "pointblank"))
+})
+

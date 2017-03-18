2a7f04f346826023550938f4f947333911ae61ad
Richard Iannone <riannone@me.com>
2017-03-17 23:26:58 -0700

    Add several interrogation tests

--- tests/testthat/test-interrogate.R
+++ tests/testthat/test-interrogate.R
@@ -360,4 +360,134 @@ test_that("Interrogating for valid row values", {
   expect_equivalent(validation$validation_set$f_passed, 0.636)
   expect_equivalent(validation$validation_set$f_failed, 0.364)
   
+  
+  # Use `col_vals_equal()` function to create
+  # a validation step, then, `interrogate()`
+  validation <-
+    create_agent() %>%
+    focus_on(
+      file_name = system.file("extdata", "small_table.csv",
+                              package = "pointblank"),
+      col_types = "TDicidlc") %>%
+    col_vals_equal(column = "d", value = 283.94) %>%
+    interrogate()
+  
+  # Expect certain values in `validation$validation_set`
+  expect_equivalent(validation$validation_set$tbl_name, "small_table")
+  expect_equivalent(validation$validation_set$db_type, "local_file")
+  expect_equivalent(validation$validation_set$assertion_type, "col_vals_equal")
+  expect_equivalent(validation$validation_set$column, "d")
+  expect_equivalent(validation$validation_set$value, 283.94)
+  expect_true(is.na(validation$validation_set$regex))
+  expect_false(validation$validation_set$all_passed)
+  expect_equivalent(validation$validation_set$n, 13)
+  expect_equivalent(validation$validation_set$n_passed, 1)
+  expect_equivalent(validation$validation_set$n_failed, 12)
+  expect_equivalent(validation$validation_set$f_passed, 0.077)
+  expect_equivalent(validation$validation_set$f_failed, 0.923)
+  expect_equivalent(validation$validation_set$report_count, 0)
+  expect_equivalent(validation$validation_set$warn_count, 1)
+  expect_equivalent(validation$validation_set$notify_count, 2)
+  expect_true(is.na(validation$validation_set$init_sql))
+  expect_true(is.na(validation$validation_set$db_cred_file_path))
+  expect_equivalent(
+    validation$validation_set$file_path,
+    system.file("extdata", "small_table.csv", package = "pointblank"))
+  
+  # Expect a single row in `validation$validation_set`
+  expect_equivalent(nrow(validation$validation_set), 1)
+  
+  # Use `col_vals_equal()` function to create
+  # a validation step (with a precondition), then,
+  # `interrogate()`
+  validation <-
+    create_agent() %>%
+    focus_on(
+      file_name = system.file("extdata", "small_table.csv",
+                              package = "pointblank"),
+      col_types = "TDicidlc") %>%
+    col_vals_equal(
+      column = "d", value = 283.94,
+      preconditions = "date > '2016-01-04'") %>%
+    interrogate()
+  
+  # Expect certain values in `validation$validation_set`
+  expect_equivalent(validation$validation_set$tbl_name, "small_table")
+  expect_equivalent(validation$validation_set$db_type, "local_file")
+  expect_equivalent(validation$validation_set$assertion_type, "col_vals_equal")
+  expect_equivalent(validation$validation_set$column, "d")
+  expect_equivalent(validation$validation_set$value, 283.94)
+  expect_true(is.na(validation$validation_set$regex))
+  expect_false(validation$validation_set$all_passed)
+  expect_equivalent(validation$validation_set$n, 11)
+  expect_equivalent(validation$validation_set$n_passed, 1)
+  expect_equivalent(validation$validation_set$n_failed, 10)
+  expect_equivalent(validation$validation_set$f_passed, 0.091)
+  expect_equivalent(validation$validation_set$f_failed, 0.909)
+  
+  
+  # Use `col_vals_not_equal()` function to create
+  # a validation step, then, `interrogate()`
+  validation <-
+    create_agent() %>%
+    focus_on(
+      file_name = system.file("extdata", "small_table.csv",
+                              package = "pointblank"),
+      col_types = "TDicidlc") %>%
+    col_vals_not_equal(column = "d", value = 283.94) %>%
+    interrogate()
+  
+  # Expect certain values in `validation$validation_set`
+  expect_equivalent(validation$validation_set$tbl_name, "small_table")
+  expect_equivalent(validation$validation_set$db_type, "local_file")
+  expect_equivalent(validation$validation_set$assertion_type, "col_vals_not_equal")
+  expect_equivalent(validation$validation_set$column, "d")
+  expect_equivalent(validation$validation_set$value, 283.94)
+  expect_true(is.na(validation$validation_set$regex))
+  expect_false(validation$validation_set$all_passed)
+  expect_equivalent(validation$validation_set$n, 13)
+  expect_equivalent(validation$validation_set$n_passed, 12)
+  expect_equivalent(validation$validation_set$n_failed, 1)
+  expect_equivalent(validation$validation_set$f_passed, 0.923)
+  expect_equivalent(validation$validation_set$f_failed, 0.077)
+  expect_equivalent(validation$validation_set$report_count, 0)
+  expect_equivalent(validation$validation_set$warn_count, 1)
+  expect_equivalent(validation$validation_set$notify_count, 2)
+  expect_true(is.na(validation$validation_set$init_sql))
+  expect_true(is.na(validation$validation_set$db_cred_file_path))
+  expect_equivalent(
+    validation$validation_set$file_path,
+    system.file("extdata", "small_table.csv", package = "pointblank"))
+  
+  # Expect a single row in `validation$validation_set`
+  expect_equivalent(nrow(validation$validation_set), 1)
+  
+  # Use `col_vals_equal()` function to create
+  # a validation step (with a precondition), then,
+  # `interrogate()`
+  validation <-
+    create_agent() %>%
+    focus_on(
+      file_name = system.file("extdata", "small_table.csv",
+                              package = "pointblank"),
+      col_types = "TDicidlc") %>%
+    col_vals_not_equal(
+      column = "d", value = 283.94,
+      preconditions = "date > '2016-01-04'") %>%
+    interrogate()
+  
+  # Expect certain values in `validation$validation_set`
+  expect_equivalent(validation$validation_set$tbl_name, "small_table")
+  expect_equivalent(validation$validation_set$db_type, "local_file")
+  expect_equivalent(validation$validation_set$assertion_type, "col_vals_not_equal")
+  expect_equivalent(validation$validation_set$column, "d")
+  expect_equivalent(validation$validation_set$value, 283.94)
+  expect_true(is.na(validation$validation_set$regex))
+  expect_false(validation$validation_set$all_passed)
+  expect_equivalent(validation$validation_set$n, 11)
+  expect_equivalent(validation$validation_set$n_passed, 10)
+  expect_equivalent(validation$validation_set$n_failed, 1)
+  expect_equivalent(validation$validation_set$f_passed, 0.909)
+  expect_equivalent(validation$validation_set$f_failed, 0.091)
 })
+

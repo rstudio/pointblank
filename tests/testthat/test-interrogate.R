23948ef3a24f5e57ee539ce53d31a8a6a1d627bb
Richard Iannone <riannone@me.com>
2017-03-18 17:42:52 -0700

    Add tests for `col_vals_not_in_set()` validation

--- tests/testthat/test-interrogate.R
+++ tests/testthat/test-interrogate.R
@@ -613,5 +613,77 @@ test_that("Interrogating for valid row values", {
   
   # Expect a single row in `validation$validation_set`
   expect_equivalent(nrow(validation$validation_set), 1)
+  
+  # Use `col_vals_not_in_set()` function to create
+  # a validation step, then, `interrogate()`
+  validation <-
+    create_agent() %>%
+    focus_on(
+      file_name = system.file("extdata", "small_table.csv",
+                              package = "pointblank"),
+      col_types = "TDicidlc") %>%
+    col_vals_not_in_set(column = "f", set = c("lower", "higher")) %>%
+    interrogate()
+  
+  # Expect certain values in `validation$validation_set`
+  expect_equivalent(validation$validation_set$tbl_name, "small_table")
+  expect_equivalent(validation$validation_set$db_type, "local_file")
+  expect_equivalent(validation$validation_set$assertion_type, "col_vals_not_in_set")
+  expect_equivalent(validation$validation_set$column, "f")
+  expect_true(is.na(validation$validation_set$value))
+  expect_true(is.na(validation$validation_set$regex))
+  expect_true(validation$validation_set$all_passed)
+  expect_equivalent(validation$validation_set$n, 13)
+  expect_equivalent(validation$validation_set$n_passed, 13)
+  expect_equivalent(validation$validation_set$n_failed, 0)
+  expect_equivalent(validation$validation_set$f_passed, 1)
+  expect_equivalent(validation$validation_set$f_failed, 0)
+  expect_equivalent(validation$validation_set$report_count, 0)
+  expect_equivalent(validation$validation_set$warn_count, 1)
+  expect_equivalent(validation$validation_set$notify_count, 2)
+  expect_true(is.na(validation$validation_set$init_sql))
+  expect_true(is.na(validation$validation_set$db_cred_file_path))
+  expect_equivalent(
+    validation$validation_set$file_path,
+    system.file("extdata", "small_table.csv", package = "pointblank"))
+  
+  # Expect a single row in `validation$validation_set`
+  expect_equivalent(nrow(validation$validation_set), 1)
+  
+  # Make another pass with the `col_vals_not_in_set()`
+  # function to create a validation step, then, perform an
+  # interrogation that fails every row
+  validation <-
+    create_agent() %>%
+    focus_on(
+      file_name = system.file("extdata", "small_table.csv",
+                              package = "pointblank"),
+      col_types = "TDicidlc") %>%
+    col_vals_not_in_set(column = "f", set = c("low", "mid", "high")) %>%
+    interrogate()
+  
+  # Expect certain values in `validation$validation_set`
+  expect_equivalent(validation$validation_set$tbl_name, "small_table")
+  expect_equivalent(validation$validation_set$db_type, "local_file")
+  expect_equivalent(validation$validation_set$assertion_type, "col_vals_not_in_set")
+  expect_equivalent(validation$validation_set$column, "f")
+  expect_true(is.na(validation$validation_set$value))
+  expect_true(is.na(validation$validation_set$regex))
+  expect_false(validation$validation_set$all_passed)
+  expect_equivalent(validation$validation_set$n, 13)
+  expect_equivalent(validation$validation_set$n_passed, 0)
+  expect_equivalent(validation$validation_set$n_failed, 13)
+  expect_equivalent(validation$validation_set$f_passed, 0)
+  expect_equivalent(validation$validation_set$f_failed, 1)
+  expect_equivalent(validation$validation_set$report_count, 0)
+  expect_equivalent(validation$validation_set$warn_count, 1)
+  expect_equivalent(validation$validation_set$notify_count, 2)
+  expect_true(is.na(validation$validation_set$init_sql))
+  expect_true(is.na(validation$validation_set$db_cred_file_path))
+  expect_equivalent(
+    validation$validation_set$file_path,
+    system.file("extdata", "small_table.csv", package = "pointblank"))
+  
+  # Expect a single row in `validation$validation_set`
+  expect_equivalent(nrow(validation$validation_set), 1)
 })
-

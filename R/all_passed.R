c3203bcc618f4db0266df7120d56ce1e822c1127
Richard Iannone <riannone@me.com>
2017-04-06 12:07:17 -0700

    Add function `all_passed`

---
+++ R/all_passed.R
@@ -0,0 +1,18 @@
+#' Did all of the validations pass?
+#' @description Given a completed validation
+#' pass, this function will return \code{TRUE}
+#' or \code{FALSE} on whether all of the
+#' validations passed without any fails. 
+#' @param agent an agent object of class
+#' \code{ptblank_agent}.
+#' @return an agent object.
+#' @export all_passed
+
+all_passed <- function(agent) {
+  
+  if (all(agent$validation_set$all_passed == TRUE)) {
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}

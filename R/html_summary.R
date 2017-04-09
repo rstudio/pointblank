4b7a27da5eddcbc2137334ad6a2fa40ac2ead6c9
Richard Iannone <riannone@me.com>
2017-04-09 09:48:25 -0700

    Add function `html_summary`

---
+++ R/html_summary.R
@@ -0,0 +1,14 @@
+#' Create an HTML summary of the validation 
+#' @description Create an HTML summary of the validation.
+#' @param reporting_object a pointblank summary object.
+#' @importFrom rmarkdown render
+#' @export html_summary
+
+html_summary <- function(reporting_object) {
+  
+  saveRDS(report_object, "summary.rds")
+  file.copy(from = system.file("report_rmd", "validation_report.Rmd", package = "pointblank"),
+            to = "./validation_report.Rmd")
+  
+  rmarkdown::render("validation_report.Rmd")
+}

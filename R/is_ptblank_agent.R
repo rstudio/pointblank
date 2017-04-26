09bd0e3419ae72bba19352110335a093a9aaa392
Richard Iannone <riannone@me.com>
2017-04-26 09:03:36 -0700

    Add function 'is_ptblank_agent'

---
+++ R/is_ptblank_agent.R
@@ -0,0 +1,12 @@
+#' Is the object a pointblank agent?
+#' @description Determines whether the object is
+#' actually a pointbank agent object (i.e., of
+#' type \code{ptblank_agent}).
+#' @return an logical value.
+#' @export is_ptblank_agent
+
+is_ptblank_agent <- function(object) {
+  
+  # Check whether the object has class `ptblank_agent`
+  ifelse(inherits(object, "ptblank_agent"), TRUE, FALSE)
+}

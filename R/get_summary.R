a0b83234beed1d8985225c3051f145ff47060da4
Richard Iannone <riannone@me.com>
2017-05-01 18:28:04 -0700

    Include fraction passed and the action

--- R/get_summary.R
+++ R/get_summary.R
@@ -1,17 +1,28 @@
-#' Create a simple summary of the
-#' interrogation
+#' Get a simple summary of the interrogation
 #' @description Gets the essential information
 #' from an agent object after an interrogation
 #' is complete.
 #' @param agent an agent object of class
 #' \code{ptblank_agent}.
 #' @return an agent object.
+#' @importFrom dplyr select mutate case_when
 #' @export get_summary
 
 get_summary <- function(agent) {
   
   if (all(agent$validation_set$all_passed %in% c(TRUE, FALSE))) {
-    return(agent$validation_set[, c(1:11)])
+    
+    validation_summary <-
+      agent$validation_set %>%
+      dplyr::select(1:11, f_passed, warn, notify) %>%
+      dplyr::mutate(
+        action = dplyr::case_when(
+           .$warn == TRUE ~ "warn",
+           .$notify == TRUE ~ "notify",
+           .$warn == FALSE & .$notify == FALSE ~ as.character(NA))) %>%
+      dplyr::select(-warn, -notify)
+    
+    return(validation_summary)
   } else {
     stop("An interrogation hasn't yet occurred.")
   }

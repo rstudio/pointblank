a5469829f5b0e29c42bc2fd4b0e1cd0da02fa2a8
Richard Iannone <riannone@me.com>
2017-05-06 21:33:32 -0700

    Add tests for `create_email_creds_file()`

--- tests/testthat/test-create_creds_file.R
+++ tests/testthat/test-create_creds_file.R
@@ -1,6 +1,6 @@
 context("Creating a credentials file")
 
-test_that("Creating a credentials file is possible", {
+test_that("Creating a database credentials file is possible", {
   
   # Create a crendentials file
   create_creds_file(
@@ -20,4 +20,27 @@ test_that("Creating a credentials file is possible", {
   expect_equivalent(creds[3], "31")
   expect_equivalent(creds[4], "user")
   expect_equivalent(creds[5], "password")
+})
+
+test_that("Creating an email credentials file is possible", {
+  
+  # Create a crendentials file
+  create_email_creds_file(
+    file = "email_creds_file.rds",
+    sender = "point@blank.org",
+    host = "smtp.blank.org",
+    port = 465,
+    user = "point@blank.org",
+    password = "password")
+  
+  # Read in the credentials file
+  email_creds <- readRDS("email_creds_file.rds")
+  
+  # Expect the same input values in the credentials file vector
+  expect_equivalent(email_creds[1], "point@blank.org")
+  expect_equivalent(email_creds[3], "465")
+  expect_equivalent(email_creds[4], "point@blank.org")
+  expect_equivalent(email_creds[5], "password")
+  expect_equivalent(email_creds[6], "TRUE")
+  expect_equivalent(email_creds[7], "TRUE")
 })

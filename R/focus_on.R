f091fd42588f15a7a04b46c765cb01b30af5e549
Richard Iannone <riannone@me.com>
2017-06-11 22:24:38 -0700

    Add examples to roxygen @examples section

--- R/focus_on.R
+++ R/focus_on.R
@@ -45,6 +45,31 @@
 #' in the Logical Plan section of the report generated
 #' by the \code{html_summary} function.
 #' @return an agent object.
+#' @examples
+#' # Create a simple data frame with a column
+#' # of numerical values
+#' df <-
+#'   data.frame(
+#'     a = c(5, 4, 3, 5, 1, 2))
+#' 
+#' # Validate that values in column `a` are
+#' # always less than 6; this requires the
+#' # use of `create_agent()` (to begin the 
+#' # validation process) and `focus_on()`
+#' # to point to the table that will undergo
+#' # validation in subsequent steps
+#' agent <-
+#'   create_agent() %>%
+#'   focus_on(tbl_name = "df") %>%
+#'   col_vals_lt(
+#'     column = "a",
+#'     value = 6) %>%
+#'   interrogate()
+#' 
+#' # Determine if this column validation has
+#' # passed by using `all_passed()`
+#' all_passed(agent)
+#' #> [1] TRUE
 #' @importFrom dplyr filter bind_rows
 #' @importFrom readr read_csv read_tsv
 #' @importFrom stringr str_split

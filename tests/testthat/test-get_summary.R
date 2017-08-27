78180d035026e868381fcfca0912f9cb2b8b3f20
Richard Iannone <riannone@me.com>
2017-08-26 21:30:28 -0700

    Modify tests based on new function behavior

--- tests/testthat/test-get_summary.R
+++ tests/testthat/test-get_summary.R
@@ -6,23 +6,23 @@ test_that("Getting a validation summary is possible", {
   library(dplyr)
   
   # Use `col_is_character()` function to create
-  # a validation step
-  validation <-
+  # a validation step and then `interrogate()`
+  agent <-
     create_agent() %>%
     focus_on(
       file_name = system.file("extdata", "small_table.csv", package = "pointblank")) %>%
     col_is_character(column = "b") %>%
     interrogate()
   
-  # Get a summary of the validation
-  summary <- get_summary(validation)
+  # Get a summary of the interrogation
+  summary <- get_interrogation_summary(agent)
   
   # Expect that the summary has specific column names
   expect_equivalent(
     colnames(summary),
     c("tbl_name", "db_type", "assertion_type", "column", "value",
       "regex", "all_passed", "n", "n_passed",
-      "f_passed", "action"))
+      "f_passed", "brief", "action"))
   
   # Expect a single row in this summary
   expect_equivalent(nrow(summary), 1)
@@ -38,14 +38,15 @@ test_that("Getting a validation summary is possible", {
   expect_is(summary$n, "numeric")
   expect_is(summary$n_passed, "numeric")
   expect_is(summary$f_passed, "numeric")
+  expect_is(summary$brief, "character")
   expect_is(summary$action, "character")
   
   # Expect an error if getting a summary without an interrogation
   expect_error(
     create_agent() %>%
-    focus_on(
-      file_name = system.file("extdata", "small_table.csv", package = "pointblank")) %>%
-    col_is_character(column = "b") %>%
-    get_summary())
+      focus_on(
+        file_name = system.file("extdata", "small_table.csv", package = "pointblank")) %>%
+      col_is_character(column = "b") %>%
+      get_interrogation_summary())
   
 })

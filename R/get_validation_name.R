019fad7ccb2e6e11e0098e67c08a472ac4bb5daa
Richard Iannone <riannone@me.com>
2017-11-13 20:40:30 -0800

    Add function 'get_validation_name'

---
+++ R/get_validation_name.R
@@ -0,0 +1,16 @@
+#' Get the pointblank validation name
+#' @description Gets the name of a pointblank
+#' validation from an agent object. This name
+#' is optionally assigned during the
+#' \code{create_agent()} call using the
+#' \code{validation_name} argument.
+#' @param agent an agent object of
+#' class \code{ptblank_agent}.
+#' @return a character object with the agent
+#' name.
+#' @export get_validation_name
+
+get_validation_name <- function(agent) {
+  
+  agent$validation_name
+}

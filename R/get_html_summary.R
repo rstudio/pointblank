b8ce1a07214095d6483ce37226a2e30b48079deb
Richard Iannone <riannone@me.com>
2017-11-13 19:57:59 -0800

    Simplify naming/locations of generated files

--- R/get_html_summary.R
+++ R/get_html_summary.R
@@ -1,39 +1,48 @@
 #' Create an HTML summary file for the
 #' interrogation
-#' @description Get the essential information
-#' from an agent object after an interrogation
-#' is complete and then generates an HTML file
-#' for a visual summary.
-#' @param agent an agent object of class
-#' \code{ptblank_agent}.
-#' @param filename an optional filename to use
-#' for the output HTML file. If not provided,
-#' the filename \code{validation_report.html}
-#' will be used.
-#' @param intro_text HTML text to be placed at
-#' the top of the summary. Can be provided
-#' as plaintext or as markdown text.
-#' @param pre_results_text HTML text to be
-#' placed just before the tables of validation
-#' results. Can be provided as plaintext or as
+#' @description Get the essential
+#' information from an agent object
+#' after an interrogation is complete
+#' and then generates an HTML file for
+#' a visual summary.
+#' @param agent an agent object of
+#' class \code{ptblank_agent}.
+#' @param output_file an optional filename
+#' to use for the output HTML file. If
+#' not provided, the filename
+#' \code{validation_report.html} will
+#' be used.
+#' @param output_dir an optional path
+#' to place the output HTML file and
+#' the subfolder of CSV data (if row
+#' sample data has been collected). If
+#' the path does not exist, the
+#' directory will be created.
+#' @param intro_text HTML text to be
+#' placed at the top of the summary.
+#' Can be provided as plaintext or as
 #' markdown text.
-#' @param footer_text HTML text to be placed in
-#' the footer of the summary. Can be provided
-#' as plaintext or as markdown text.
-#' @param output_dir an optional path to place
-#' the output HTML file and subfolder of CSV
-#' data (if that exists). The path must exist
-#' otherwise no movement of files will occur.
+#' @param pre_results_text HTML text
+#' to be placed just before the tables
+#' of validation results. Can be
+#' provided as plaintext or as markdown
+#' text.
+#' @param footer_text HTML text to be
+#' placed in the footer of the summary.
+#' Can be provided as plaintext or as
+#' markdown text.
+#' @return an agent object.
 #' @importFrom rmarkdown render
+#' @importFrom stringr str_replace_all str_detect
 #' @importFrom commonmark markdown_html
 #' @export get_html_summary
 
 get_html_summary <- function(agent,
-                             filename = NULL,
+                             output_file = NULL,
+                             output_dir = NULL,
                              intro_text = NULL,
                              pre_results_text = NULL,
-                             footer_text = NULL,
-                             output_dir = NULL) {
+                             footer_text = NULL) {
   
   if (!inherits(agent, "ptblank_agent")) {
     stop("The object provided must be a valid `ptblank_agent` object.")
@@ -64,10 +73,32 @@ get_html_summary <- function(agent,
     invisible(file.remove("agent.rds"))
   }
   
-  if (dir.exists("_csv_data")) {
+  if (is.null(output_file)) {
+    output_file <- "validation_report.html"
+  }
+  
+  if (is.null(output_dir)) {
+    output_dir <- getwd()
+  }
+  
+  # If the `output_file` file does not have
+  # an `.html` extension, then append that
+  # to the file name
+  if (stringr::str_detect(string = output_file, pattern = "\\.html$") == FALSE) {
+    output_file <- paste0(output_file, ".html")
+  }
+    
+  # Generate the CSV subfolder name based
+  # on `agent$validation_name`
+  csv_subfolder_name <-
+    gsub("(-|:)", "_", agent$validation_name)
+  
+  # Remove any previously generated subfolder
+  # directory if one exists
+  if (dir.exists(csv_subfolder_name)) {
     invisible(
       unlink(
-        x = "_csv_data",
+        x = csv_subfolder_name,
         recursive = TRUE,
         force = TRUE))
   }
@@ -78,7 +109,9 @@ get_html_summary <- function(agent,
   # Copy the validation report .Rmd file from
   # `/inst/report_rmd` to the working directory
   file.copy(
-    from = system.file("report_rmd", "validation_report.Rmd", package = "pointblank"),
+    from = system.file(
+      "report_rmd", "validation_report.Rmd",
+      package = "pointblank"),
     to = "./validation_report.Rmd",
     overwrite = TRUE)
   
@@ -87,7 +120,9 @@ get_html_summary <- function(agent,
   if (!is.null(intro_text)) {
     if (inherits(intro_text, "character")) {
       cat(
-        commonmark::markdown_html(intro_text),
+        stringr::str_replace_all(
+          commonmark::markdown_html(intro_text),
+          "\n", ""),
         file = "include_intro.html")
     }
   }
@@ -97,7 +132,9 @@ get_html_summary <- function(agent,
   if (!is.null(pre_results_text)) {
     if (inherits(pre_results_text, "character")) {
       cat(
-        commonmark::markdown_html(pre_results_text),
+        stringr::str_replace_all(
+          commonmark::markdown_html(pre_results_text),
+          "\n", ""),
         file = "include_pre_results.html")
     }
   }
@@ -107,7 +144,9 @@ get_html_summary <- function(agent,
   if (!is.null(footer_text)) {
     if (inherits(footer_text, "character")) {
       cat(
-        commonmark::markdown_html(footer_text),
+        stringr::str_replace_all(
+          commonmark::markdown_html(footer_text),
+          "\n", ""),
         file = "include_footer.html")
     }
   } else {
@@ -118,10 +157,9 @@ get_html_summary <- function(agent,
   suppressMessages(
     rmarkdown::render(
       input = "validation_report.Rmd",
-      output_format = "html_document"))
-  
-  # Get the default output filename
-  output_file <- "validation_report.html"
+      output_format = "html_document",
+      output_dir = output_dir,
+      output_file = output_file))
   
   #
   # Perform clean up of working directory
@@ -155,7 +193,7 @@ get_html_summary <- function(agent,
     
     # Remove the image files
     invisible(file.remove(temp_images_files))
-
+    
     # Remove the empty parent directory
     invisible(file.remove("temporary_images"))
   }
@@ -173,40 +211,14 @@ get_html_summary <- function(agent,
     invisible(file.remove("temporary_images_plan"))
   }
   
-  # Save file with a specified file name
-  if (!is.null(filename)) {
-    if (inherits(filename, "character")) {
-      invisible(
-        file.rename(
-          from = "validation_report.html",
-          to = filename))
-      
-      output_file <- filename
-    }
+  # Move the subfolder of CSV data if
+  # it has been made
+  if (dir.exists(csv_subfolder_name)) {
+    
+    file.rename(
+      from = csv_subfolder_name,
+      to = gsub("//", "/", paste0(output_dir, "/", csv_subfolder_name)))
   }
   
-  # Move file to a specified directory
-  if (!is.null(output_dir)) {
-    
-    if (dir.exists(output_dir)) {
-      invisible(
-        file.copy(
-          from = gsub("//", "/", paste0(getwd(), "/", output_file)),
-          to = gsub("//", "/", paste0(output_dir, "/", output_file)),
-          overwrite = TRUE))
-      
-      # Remove the file from the working directory
-      invisible(file.remove(output_file))
-      
-      # Move the subfolder of CSV data if
-      # it has been made
-      if (dir.exists("_csv_data")) {
-        
-        file.rename(
-          from = "_csv_data",
-          to = gsub("//", "/", paste0(output_dir, "/_csv_data")))
-        
-      }
-    }
-  }
+  agent
 }

4b62026ff77422977ec09992c6a2f1620870b416
Richard Iannone <riannone@me.com>
2020-01-08 10:12:09 -0500

    Add documentation

--- R/datasets.R
+++ R/datasets.R
@@ -1,15 +1,21 @@
 #' A small table that useful for testing
 #'
 #' This is a small table with a few different types of columns. It's probably
-#' just useful when testing the functions from **pointblank**.
+#' just useful when testing the functions from **pointblank**. Rows 9 and 10 are
+#' exact duplicates. The `c` column contains two `NA` values.
 #'
-#' @format A tibble with 12470 rows and 5 variables:
+#' @format A tibble with 13 rows and 8 variables:
 #' \describe{
-#' \item{country_name}{Name of the country}
-#' \item{country_code_2}{The 2-letter ISO 3166-1 country code}
-#' \item{country_code_3}{The 3-letter ISO 3166-1 country code}
-#' \item{year}{The year for the population estimate}
-#' \item{population}{The population estimate, midway through the year}
+#' \item{date_time}{A date-time column (of the `POSIXct` class) with dates that
+#' correspond exactly to those in the `date` column. Time values are somewhat
+#' randomized but all 'seconds' values are `00`.}
+#' \item{date}{A `Date` column with dates from `2016-01-04` to `2016-01-30`.}
+#' \item{a}{An `integer` column with values ranging from `1` to `8`.}
+#' \item{b}{A `character` column with values that adhere to a common pattern.}
+#' \item{c}{An `integer` column with values ranging from `2` to `9`. Contains two `NA` values.}
+#' \item{d}{A numeric column with values ranging from `108` to `10000`.}
+#' \item{e}{A `logical` column.}
+#' \item{f}{A `character` column with `"low"`, `"mid"`, and `"high"` values.}
 #' }
 #'
 #' @examples

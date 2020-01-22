fe71773285953efaa450f56a2c2150b721b8c192
Richard Iannone <riannone@me.com>
2020-01-22 00:44:37 -0500

    Create 02-small_table_sqlite.R

---
+++ data-raw/02-small_table_sqlite.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(DBI)
+library(RSQLite)
+
+small_table <-
+  readr::read_csv(
+    file = "data-raw/small_table.csv",
+    col_types = "TDicddlc"
+  )
+
+con <- DBI::dbConnect(RSQLite::SQLite(), dbname = "inst/small_table.db")
+
+DBI::dbWriteTable(
+  conn = con,
+  name = "small_table",
+  value = small_table
+)

96cdfd8e22492c8e1f309dd5b76e296bd024f55e
Richard Iannone <riannone@me.com>
2020-02-12 01:57:53 -0500

    Rewrite print method

--- R/print.R
+++ R/print.R
@@ -1,8 +1,9 @@
 #' Print the agent information to the console
 #'
-#' This function will allow the agent to provide a short message in the console.
+#' This function will allow the agent to print a useful report.
 #' 
 #' @param x An agent object of class `ptblank_agent`.
+#' @param ... Any additional parameters.
 #' 
 #' @keywords internal
 #' @export
@@ -10,54 +11,7 @@ print.ptblank_agent <- function(x, ...) {
   
   # nocov start 
   
-  args <- list(...)
-  args <- NULL
-  
-  # Get the console width
-  console_width <- getOption("width")
-
-  # Generate the complete statement for printing
-  if (is_agent_empty(x)) {
-    
-    print_stmt <- paste0("pointblank agent // <", x$name, ">")
-    
-  } else {
-    
-    print_stmt <-
-      paste0(
-        "pointblank agent // <", x$name, ">", "\n", "\n",
-        "number of validation steps: ", number_of_validation_steps(x), "\n"
-      )
-    
-    if (has_agent_intel(x)) {
-      
-      passing_steps <-
-        length(which(x$validation_set$all_passed == TRUE))
-      
-      failing_steps <-
-        length(which(x$validation_set$all_passed == FALSE))
-      
-      print_stmt <-
-        paste0(
-          print_stmt, "\n",
-          "interrogation (",
-          interrogation_time(x), ") resulted in:", "\n",
-          "  - ", 
-          ifelse(passing_steps > 0, as.character(passing_steps), "no"),
-          " passing validation",
-          ifelse(passing_steps == 1, "", "s"),
-          "\n",
-          "  - ", 
-          ifelse(failing_steps > 0, as.character(failing_steps), "no"),
-          " failing validation",
-          ifelse(failing_steps == 1, "", "s"),
-          "   ",
-          "more info: `get_agent_report()`\n"
-        )
-    }
-  }
-  
-  cat(print_stmt)
+  print(get_agent_report(x))
   
   # nocov end 
 }

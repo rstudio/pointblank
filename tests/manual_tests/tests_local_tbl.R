5b96b2db191c4c6bf27e91600026d2fd043ecfbf
Richard Iannone <riannone@me.com>
2020-02-14 19:57:52 -0500

    Update tests_local_tbl.R

--- tests/manual_tests/tests_local_tbl.R
+++ tests/manual_tests/tests_local_tbl.R
@@ -12,10 +12,14 @@ agent <-
   col_vals_between(vars(c), left = vars(a), right = vars(d), na_pass = TRUE)
 
 agent <- agent %>% interrogate()
-
 agent
 
-extract_3 <- agent %>% get_data_extracts(i = 3)
-extract_3
+extracts <- agent %>% get_data_extracts()
+extracts
 
-agent %>% get_sundered_data()
+x_list <- get_agent_x_list(agent)
+x_list
+
+email_blast_preview(agent)
+
+get_sundered_data(agent)

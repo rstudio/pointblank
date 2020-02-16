b10381e6ad3ccab6ba35dc7e0ecd35fdfddc3493
Richard Iannone <riannone@me.com>
2020-02-16 14:28:27 -0500

    Update tests_simple.R

--- tests/manual_tests/tests_simple.R
+++ tests/manual_tests/tests_simple.R
@@ -1,8 +1,12 @@
 library(pointblank)
 library(tidyverse)
 
+al <- action_levels(warn_at = 3)
+
 small_table %>%
-  col_vals_gt(
-    vars(d), 1000,
-    actions = action_levels(warn_at = 3)
-  )
+  col_vals_gt(vars(d), 1000, actions = al) %>%
+  col_vals_gt(vars(b), vars(g), na_pass = TRUE, actions = al) %>%
+  rows_distinct(vars(d, e), actions = al) %>%
+  rows_distinct(vars(a, f), actions = al) %>%
+  col_vals_gt(vars(d), 100, actions = al) %>%
+  col_vals_equal(vars(d), vars(d), na_pass = TRUE, actions = al)

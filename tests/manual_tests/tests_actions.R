d6b329e2504579e2c94dbced08e03fae8ac1dca7
Richard Iannone <riannone@me.com>
2020-02-16 14:32:57 -0500

    Update tests_actions.R

--- tests/manual_tests/tests_actions.R
+++ tests/manual_tests/tests_actions.R
@@ -1,6 +1,6 @@
 library(pointblank)
-library(tidyverse)
 library(log4r)
+library(glue)
 
 # Create a log4r `logger` object
 logger <- logger("WARN", appenders = file_appender("log_file"))
@@ -8,27 +8,23 @@ logger <- logger("WARN", appenders = file_appender("log_file"))
 # Create a custom logging function to react to any `warn`
 # state. The `.vars_list` list is available in the context
 # where the function will be evaluated
-log4r_warn <- function(vl) {
+log4r_warn <- function(x) {
     log4r::warn(logger, glue::glue(
-      "Step {vl$i} exceeded the `warn` threshold (f_failed = {vl$f_failed}) ['{vl$name}']"
+      "Step {x$i} exceeded the `warn` threshold (f_failed = {x$f_failed}) ['{x$name}']"
     ))
 }
 
 agent <-
-  create_agent(tbl = small_table, name = "small_table_tests") %>%
-  col_vals_gt(
-    vars(d), 1000,
-    actions = action_levels(warn_at = 3, fns = list(warn = ~logger_warn(.vars_list))
-    )
+  create_agent(
+    tbl = small_table,
+    name = "small_table_tests",
+    actions = action_levels(warn_at = 3, fns = list(warn = ~log4r_warn(x)))
   ) %>%
-  col_vals_in_set(
-    vars(f), c("low", "high"),
-    actions = action_levels(warn_at = 0.1, fns = list(warn = ~logger_warn(.vars_list))
-    )
-  ) %>%
+  col_vals_gt(vars(d), 1000) %>%
+  col_vals_in_set(vars(f), c("low", "high")) %>%
   interrogate()
 
-agent %>% get_agent_report()
+agent
 
 agent %>% get_data_extracts(i = 1)
 

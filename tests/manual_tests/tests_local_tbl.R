20529e7bb73b35ea1c7c73a92362534143ce4f99
Richard Iannone <riannone@me.com>
2020-02-17 15:01:47 -0500

    Update tests_local_tbl.R

--- tests/manual_tests/tests_local_tbl.R
+++ tests/manual_tests/tests_local_tbl.R
@@ -1,12 +1,13 @@
 library(pointblank)
 library(tidyverse)
 
+al <- action_levels(warn_at = 0.1, stop_at = 0.2)
+
 agent <-
-  create_agent(tbl = small_table) %>%
+  create_agent(tbl = small_table, actions = al) %>%
   col_vals_gt(vars(date_time), vars(date), na_pass = TRUE) %>%
   col_vals_gt(vars(b), vars(g), na_pass = TRUE) %>%
-  rows_distinct(vars(d, e)) %>%
-  rows_distinct(vars(a, f)) %>%
+  rows_distinct() %>%
   col_vals_gt(vars(d), 100) %>%
   col_vals_equal(vars(d), vars(d), na_pass = TRUE) %>%
   col_vals_between(vars(c), left = vars(a), right = vars(d), na_pass = TRUE)

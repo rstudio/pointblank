bd28c2ab5f89d8ad52e2bff78be800ebf8d582be
Richard Iannone <riannone@me.com>
2020-03-21 15:22:27 -0400

    Add `scan_data()` fcn and print methods

---
+++ R/scan_data.R
@@ -0,0 +1,48 @@
+#' Thoroughly scan the table data so as to understand it better
+#' 
+#' Generates an HTML report that scours the input table data. Before calling up
+#' an *agent* to validate the data, it's a good idea to understand the data with
+#' some level of precision. Make this the initial step of a well-balanced *data
+#' quality reporting* workflow. The reporting output contains several sections
+#' to make everything more digestible.
+#' 
+#' @inheritParams create_agent
+#' 
+#' @export
+scan_data <- function(tbl) {
+
+  examination_page_html <- build_examination_page(data = tbl)
+  examination_page_html
+}
+
+#' Print the reporting produced by [scan_data()]
+#'
+#' This facilitates printing of the HTML report to the R console.
+#'
+#' @param x An object of class `examination_page`.
+#' @param ... Any additional parameters.
+#' @param view The value for `print()`s `browse` argument.
+#'
+#' @keywords internal
+#'
+#' @export
+print.examination_page <- function(x) {
+  
+  # Use `html_print()` to print to the console
+  htmltools::html_print(x)
+}
+
+#' Knit print the reporting produced by [scan_data()] 
+#'
+#' This facilitates printing of the HTML report within a knitr code chunk.
+#'
+#' @param x An object of class `examination_page`.
+#' @param ... Any additional parameters.
+#'
+#' @keywords internal
+#' @noRd
+knit_print.examination_page <- function(x, ...) {
+  
+  # Use `knit_print()` to print in a code chunk
+  knitr::knit_print(x, ...)
+}

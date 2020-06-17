0d8debd5768be51d3350ac656dae6081cf04e520
Mauricio Vargas <mvargas@dcc.uchile.cl>
2020-06-17 14:00:47 -0400

    moves dittodb testing to new branch

--- tests/testthat/aedes_aegypti_core_55_1d/SELECT-2e84e7.R
+++
@@ -1,3 +0,0 @@
-structure(list(asm_seq_region_id = 1L, cmp_seq_region_id = 39968L, 
-    asm_start = 1L, asm_end = 5950L, cmp_start = 1L, cmp_end = 5950L, 
-    ori = 1L), class = "data.frame", row.names = c(NA, -1L))
--- tests/testthat/trade_statistics/dbListTables.R
+++
@@ -1,5 +0,0 @@
-c("attributes_sections_names", "attributes_sections_colors", 
-"attributes_countries", "attributes_products_shortnames", "hs07_yr", 
-"hs07_yrp", "hs07_yrpc", "hs07_yc", "attributes_groups", "hs07_yrc", 
-"attributes_sections", "attributes_sections_shortnames", "attributes_products_names"
-)

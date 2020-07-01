7bc6b08758c8c19d78c6609a274be7914e5747b7
Richard Iannone <riannone@me.com>
2020-07-01 09:12:05 -0400

    Update tests_step_id.R

--- tests/manual_tests/tests_step_id.R
+++ tests/manual_tests/tests_step_id.R
@@ -2,7 +2,6 @@ library(pointblank)
 
 al <- action_levels(warn_at = 0.1, stop_at = 0.2)
 
-
 # Automatically generated `step_id` values
 agent <-
   small_table %>%
@@ -14,7 +13,8 @@ agent <-
 agent$validation_set$step_id
 # [1] "0001" "0002" "0003" "0004" "0005" "0006"
 
-# Automatically generated `step_id` values
+# Mixture of automatically generated `step_id` values and
+# user-provided values
 agent <-
   small_table %>%
   create_agent(actions = al) %>%
@@ -24,3 +24,12 @@ agent <-
 
 agent$validation_set$step_id
 # [1] "0001" "0002" "exists.0001" "exists.0002" "exists.0003" "exists.0004"
+
+# This validation plan will stop since duplicate `step_id` values
+# are seen across validation steps
+agent <-
+  small_table %>%
+  create_agent(actions = al) %>%
+  col_vals_gt(vars(date_time), vars(date), na_pass = TRUE, step_id = "dupe") %>%
+  col_exists(vars(a), step_id = "dupe")
+

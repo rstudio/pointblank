1ace941d6763caf2fc7e92c0ab25fb4cb57da41d
Richard Iannone <riannone@me.com>
2020-06-30 21:56:40 -0400

    Create tests_step_id.R

---
+++ tests/manual_tests/tests_step_id.R
@@ -0,0 +1,26 @@
+library(pointblank)
+
+al <- action_levels(warn_at = 0.1, stop_at = 0.2)
+
+
+# Automatically generated `step_id` values
+agent <-
+  small_table %>%
+  create_agent(actions = al) %>%
+  col_vals_gt(vars(date_time), vars(date), na_pass = TRUE) %>%
+  col_vals_gt(vars(b), vars(g), na_pass = TRUE) %>%
+  col_exists(vars(a, b, c, d))
+
+agent$validation_set$step_id
+# [1] "0001" "0002" "0003" "0004" "0005" "0006"
+
+# Automatically generated `step_id` values
+agent <-
+  small_table %>%
+  create_agent(actions = al) %>%
+  col_vals_gt(vars(date_time), vars(date), na_pass = TRUE) %>%
+  col_vals_gt(vars(b), vars(g), na_pass = TRUE) %>%
+  col_exists(vars(a, b, c, d), step_id = "exists")
+
+agent$validation_set$step_id
+# [1] "0001" "0002" "exists.0001" "exists.0002" "exists.0003" "exists.0004"

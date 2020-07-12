54f9cf873d556cd376458a16e5761c55c4223f01
Richard Iannone <riannone@me.com>
2020-07-12 11:32:21 -0400

    Update tests_yaml.R

--- tests/manual_tests/tests_yaml.R
+++ tests/manual_tests/tests_yaml.R
@@ -4,12 +4,18 @@ al <- action_levels(warn_at = 0.1, stop_at = 0.2)
 
 agent <- 
   create_agent(tbl = small_table, read_fn = ~ small_table, actions = al) %>%
-  col_vals_in_set(vars(f), set = c("low", "mid", "high")) %>%
+  col_vals_in_set(
+    vars(f), set = c("low", "mid", "high"),
+    actions = action_levels(warn_at = 3, notify_at = 5)
+  ) %>%
   col_vals_between(vars(a), 2, 8) %>%
   col_vals_gt(vars(d), 100) %>%
   col_vals_lt(vars(a), vars(d), na_pass = TRUE, preconditions = ~ . %>% dplyr::filter(a > 3)) %>%
   col_vals_equal(vars(d), vars(d), na_pass = TRUE) %>%
-  col_vals_null(vars(c)) %>%
+  col_vals_null(
+    vars(c),
+    actions = action_levels(warn_at = 3, fns = list(warn = ~print(Sys.time())))
+  ) %>%
   col_vals_regex(vars(b), regex = "[0-9]-[a-z]{3}-[0-9]{3}") %>%
   col_is_character(vars(b)) %>%
   col_exists(vars(a, b)) %>%

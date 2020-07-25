8f1897ff5dcc57e0a54408c1790565e50754de09
Pachamaltese <mvargas@dcc.uchile.cl>
2020-07-25 17:21:22 -0400

    tests using dittodb from CRAN

---
+++ tests/testthat/trade_statistics/SELECT-651291.R
@@ -0,0 +1,4 @@
+structure(list(year = integer(0), reporter_iso = character(0), 
+    partner_iso = character(0), export_value_usd = numeric(0), 
+    import_value_usd = numeric(0)), class = "data.frame", row.names = c(NA, 
+0L))
---
+++ tests/testthat/trade_statistics/select-00b826.R
@@ -0,0 +1,4 @@
+structure(list(column_name = c("year", "reporter_iso", "partner_iso", 
+"export_value_usd", "import_value_usd"), data_type = c("integer", 
+"character varying", "character varying", "numeric", "numeric"
+)), class = "data.frame", row.names = c(NA, -5L))

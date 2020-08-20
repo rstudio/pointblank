c99cd208c6b0c3096cbeaff688a87b64b7c133cf
Mauricio Vargas <mvargas@dcc.uchile.cl>
2020-08-19 23:56:57 -0400

    skips mocked tests on solaris

--- tests/testthat/test-mysql.R
+++ tests/testthat/test-mysql.R
@@ -1,6 +1,9 @@
 test_that("pointblank agent works with dittodb-mocked MySQL database connection", {
   # Create a connection to the `aedes_aegypti_core_55_1d`
   # database hosted publicly at "ensembldb.ensembl.org"
+  
+  testthat::skip_on_os("solaris")
+  
   dittodb::with_mock_db({
     # start_db_capturing()
     con <- DBI::dbConnect(

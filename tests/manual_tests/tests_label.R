b633b289de801b715a8aab90441f18a5a2e14e6c
Richard Iannone <riannone@me.com>
2020-08-25 23:19:03 -0400

    Update tests_label.R

--- tests/manual_tests/tests_label.R
+++ tests/manual_tests/tests_label.R
@@ -7,7 +7,7 @@ agent <-
   create_agent(actions = al) %>%
   col_vals_gt(vars(date_time), vars(date), na_pass = TRUE) %>%
   col_vals_gt(vars(b), vars(g), na_pass = TRUE, label = "b > g") %>%
-  rows_distinct(vars(d, e, f), label = "Distinct row across 'd', 'e', 'f'") %>%
+  rows_distinct(vars(d, e, f), label = "Distinct rows across 'd', 'e', and 'f'") %>%
   col_is_integer(vars(a), label = "`a` must be an integer") %>%
   interrogate()
 

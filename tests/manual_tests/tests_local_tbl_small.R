33d48921858ae3c161a10b500bab50f478489d6f
Richard Iannone <riannone@me.com>
2020-09-02 17:06:25 -0400

    Update tests_local_tbl_small.R

--- tests/manual_tests/tests_local_tbl_small.R
+++ tests/manual_tests/tests_local_tbl_small.R
@@ -5,7 +5,7 @@ al <- action_levels(warn_at = 0.1, stop_at = 0.2)
 agent <-
   small_table %>%
   create_agent(
-    tbl_name = "`pointblank::small_table`",
+    tbl_name = "pointblank::small_table",
     label = "A local table test.",
     actions = al
   ) %>%

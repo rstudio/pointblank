3dadec22dd98dc65b8b6e0b59e60c033bbb0c7bd
Richard Iannone <riannone@me.com>
2020-09-10 12:43:59 -0400

    Update tests_local_tbl_large.R

--- tests/manual_tests/tests_local_tbl_large.R
+++ tests/manual_tests/tests_local_tbl_large.R
@@ -1,31 +1,9 @@
 library(pointblank)
 library(intendo)
 
-# Make a vector of known product types
-intendo_products <-
-  c(
-    "gems1", "gems2", "gems3", "gems4", "gems5",
-    "gold1", "gold2", "gold3", "gold4", "gold5", "gold6", "gold7",
-    "offer1", "offer2", "offer3", "offer4", "offer5",
-    "ad_5sec", "pass"
-  )
-
-# Generate an expected schema for the table
-revenue_tbl_schema <- 
-  col_schema(
-    user_id = "character",
-    session_id = "character",
-    time = c("POSIXct", "POSIXt"),
-    name = "character",
-    size = "character",
-    type = "character",
-    price = "numeric",
-    revenue = "numeric"
-  )
-
 # Create an `action_levels` object, setting
 # *warn* and *stop* thresholds at 0.01 and 0.10
-al <- action_levels(warn_at = 0.01, stop_at = 0.10, notify_at = 0.15)
+al <- action_levels(warn_at = 0.01, stop_at = 0.10)
 
 agent <-
   create_agent(
@@ -50,22 +28,32 @@ agent <-
   ) %>%
   col_vals_in_set(
     vars(type),
-    set = c(
-      "ad", "currency", "season_pass",
-      "offer_agent", NA
-    ),
+    set = c("ad", "currency", "season_pass", "offer_agent", NA)
   ) %>%
   col_vals_in_set(
-    vars(name), set = intendo_products
+    vars(name),
+    set = c(
+        "gems1", "gems2", "gems3", "gems4", "gems5",
+        "gold1", "gold2", "gold3", "gold4", "gold5", "gold6", "gold7",
+        "offer1", "offer2", "offer3", "offer4", "offer5",
+        "ad_5sec", "pass"
+    )
   ) %>%
-  col_vals_regex(
-    vars(user_id), regex = "[A-Y]{12}"
-  ) %>%
-  col_vals_regex(
-    vars(session_id), regex = "[A-Z]{5}_[a-z]{8}"
-  ) %>%
+  col_vals_regex(vars(user_id), regex = "[A-Y]{12}") %>%
+  col_vals_regex(vars(session_id), regex = "[A-Z]{5}_[a-z]{8}") %>%
   col_is_character(vars(user_id, session_id)) %>%
-  col_schema_match(schema = revenue_tbl_schema) %>%
+  col_schema_match(
+    schema = col_schema(
+      user_id = "character",
+      session_id = "character",
+      time = c("POSIXct", "POSIXt"),
+      name = "character",
+      size = "character",
+      type = "character",
+      price = "numeric",
+      revenue = "numeric"
+    )
+  ) %>%
   interrogate()
 
 agent

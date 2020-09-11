efcd2a34ee6408ba74a72e86eeebfc20a71b576e
Richard Iannone <riannone@me.com>
2020-09-10 23:36:43 -0400

    Re-write the `meta_yaml_write()` function

--- R/yaml_meta_write.R
+++ R/yaml_meta_write.R
@@ -98,7 +98,7 @@
 #' #> )
 #' 
 #' @export
-meta_yaml_write <- function(x = NULL,
+meta_yaml_write <- function(metadata,
                             filename,
                             path = NULL) {
 
@@ -108,68 +108,8 @@ meta_yaml_write <- function(x = NULL,
   
   filename <- fs::path_expand(filename)
   
-  # If `x` is a table, generate an agent
-  if (inherits(x, "data.frame") ||
-      inherits(x, "tbl_dbi") ||
-      inherits(x, "tbl_spark")) {
-    
-    tbl_name <- deparse(match.call()$x)
-    if (tbl_name == ".") {
-      tbl_name <- NA_character_
-    }
-    
-    x <- create_agent(tbl = x, tbl_name = tbl_name)
-  }
-  
-  # Using an agent for `x`
-  if (inherits(x, "ptblank_agent")) {
-    
-    label <- x$label
-    table.name <- x$tbl_name
-    table.type <- x$tbl_src
-    
-    column_names <- x$col_names
-    column_types_r <- x$col_types
-    table.columns <- length(column_names)
-    
-    tbl <- x$tbl
-    
-    table.rows <-
-      tbl %>%
-      dplyr::count(name = "n") %>%
-      dplyr::pull(n) %>%
-      as.integer()
-    
-    column_list <-
-      list(columns = col_schema(.tbl = tbl) %>% unclass() %>% lapply(as.list))
-    
-    for (i in seq_along(column_names)) {
-      
-      column_list[["columns"]][[column_names[i]]] <- 
-        list(`_type` = paste(
-          unlist(column_list[["columns"]][[column_names[i]]]),
-          collapse = ", "
-        ))
-    }
-  }
-  
-  # Generate a list to be transformed to metadata YAML
-  meta_yaml_list <-
-    c(
-      list(
-        label = label,
-        table = list(
-          name = table.name,
-          `_columns` = table.columns,
-          `_rows` = table.rows,
-          `_type` = table.type
-        )
-      ),
-      column_list
-    )
-  
   yaml::write_yaml(
-    x = meta_yaml_list,
+    x = metadata$metadata,
     file = filename,
     handlers = list(
       logical = function(x) {
@@ -179,6 +119,4 @@ meta_yaml_write <- function(x = NULL,
       }
     )
   )
-  
-  get_metadata_report(path = filename)
 }

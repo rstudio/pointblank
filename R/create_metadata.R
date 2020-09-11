3e931b98ee010255f262190cf4bc0a6427f6b9f1
Richard Iannone <riannone@me.com>
2020-09-10 23:35:34 -0400

    Add the `create_metadata()` function

---
+++ R/create_metadata.R
@@ -0,0 +1,107 @@
+
+
+#' @export
+create_metadata <- function(tbl = NULL,
+                            read_fn = NULL,
+                            agent = NULL,
+                            tbl_name = NULL,
+                            metadata_id = NULL,
+                            label = NULL,
+                            lang = NULL,
+                            locale = NULL) {
+  
+  # Generate a label if none provided
+  if (is.null(label)) {
+    label <- paste0("[", gsub(" ", "|", as.character(Sys.time())), "]")
+  }
+ 
+  # Set the `locale` to the `lang` value if `locale` isn't set
+  if (is.null(locale)) locale <- lang
+  
+  # If nothing is provided for either `tbl`, `read_fn`, or `agent`,
+  # this function needs to be stopped
+  if (is.null(tbl) && is.null(read_fn) && is.null(agent)) {
+    
+    stop(
+      "A table object, table-reading function, or agent must be supplied:\n",
+      " * Use a table object in the `tbl` argument.\n",
+      " * Or supply a table-reading function in `read_fn`.\n",
+      " * Or even an agent with some connection to a table.",
+      call. = FALSE
+    )
+  }
+  
+  # Stop function if both a table and an agent are provided 
+  if (!is.null(tbl) && !is.null(agent)) {
+    stop("A `tbl` and a `agent` cannot both be provided.", call. = FALSE)
+  }
+  
+  if (!is.null(tbl) || !is.null(read_fn)) {
+    
+    x <- 
+      create_agent(
+        tbl = tbl,
+        read_fn = read_fn,
+        tbl_name = tbl_name
+      )
+    
+  } else {
+    x <- agent
+  }
+  
+  label <- x$label
+  table.name <- x$tbl_name
+  table.type <- x$tbl_src
+  
+  column_names <- x$col_names
+  column_types_r <- x$col_types
+  table.columns <- length(column_names)
+  
+  tbl <- x$tbl
+  
+  table.rows <- dplyr::count(tbl, name = "n") %>% dplyr::pull(n)
+  
+  column_list <- list(columns = lapply(col_schema(.tbl = tbl), as.list))
+  
+  for (i in seq_along(column_names)) {
+    
+    column_list[["columns"]][[column_names[i]]] <- 
+      list(`_type` = paste(
+        unlist(column_list[["columns"]][[column_names[i]]]),
+        collapse = ", "
+      ))
+  }
+  
+  metadata_list <-
+    c(
+      list(
+        label = label,
+        table = list(
+          name = table.name,
+          `_columns` = table.columns,
+          `_rows` = table.rows,
+          `_type` = table.type
+        )
+      ),
+      column_list
+    )
+    
+  # Create the metadata list object
+  metadata <-
+    list(
+      read_fn = read_fn,
+      tbl_name = table.name,
+      label = label,
+      lang = lang,
+      locale = locale,
+      metadata = metadata_list
+    )
+  
+  # Assign the class attribute value `ptblank_metadata` to
+  # the `metadata` object
+  attr(metadata, "class") <- "ptblank_metadata"
+  
+  metadata
+}
+
+

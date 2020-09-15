27583ef5bbe80ab9f02ca795a05989397aaf5204
Richard Iannone <riannone@me.com>
2020-09-15 00:09:01 -0400

    Prefer use of data obtainable from a `read_fn`

--- R/create_metadata.R
+++ R/create_metadata.R
@@ -75,11 +75,14 @@ create_metadata <- function(tbl = NULL,
                             label = NULL,
                             lang = NULL,
                             locale = NULL) {
+
+  read_fn_given <- !is.null(read_fn)
   
   # Generate a label if none provided
-  if (is.null(label)) {
-    label <- paste0("[", gsub(" ", "|", as.character(Sys.time())), "]")
-  }
+  label <- generate_label(label = label)
+  
+  # Normalize the reporting language identifier and stop if necessary
+  lang <- normalize_reporting_language(lang)
  
   # Set the `locale` to the `lang` value if `locale` isn't set
   if (is.null(locale)) locale <- lang
@@ -102,6 +105,36 @@ create_metadata <- function(tbl = NULL,
     stop("A `tbl` and a `agent` cannot both be provided.", call. = FALSE)
   }
   
+  # Try to infer the table name if one isn't
+  # explicitly given in `tbl_name`
+  if (!is.null(tbl) && is.null(tbl_name)) {
+    tbl_name <- deparse(match.call()$tbl)
+    if (tbl_name == ".") {
+      tbl_name <- NA_character_
+    }
+  } 
+  if (is.null(tbl_name)) {
+    tbl_name <- NA_character_
+  }
+  
+  # Prefer reading a table from a `read_fn` if it's available
+  # TODO: Verify that the table is a table object
+  # and provide an error if it isn't
+  if (!is.null(read_fn)) {
+    if (inherits(read_fn, "function")) {
+      tbl <- rlang::exec(read_fn)
+    } else if (rlang::is_formula(read_fn)) {
+      tbl <- read_fn %>% rlang::f_rhs() %>% rlang::eval_tidy()
+    } else {
+      stop(
+        "The `read_fn` object must be a function or an R formula.\n",
+        "* A function can be made with `function()` {<table reading code>}.\n",
+        "* An R formula can also be used, with the expression on the RHS.",
+        call. = FALSE
+      )
+    }
+  }
+  
   if (!is.null(tbl) || !is.null(read_fn)) {
     
     x <- 
@@ -122,13 +155,13 @@ create_metadata <- function(tbl = NULL,
   
   column_names <- x$col_names
   column_types_r <- x$col_types
+  
+  .tbl <- x$tbl
+  
   table.columns <- length(column_names)
+  table.rows <- dplyr::count(.tbl, name = "n") %>% dplyr::pull(n)
   
-  tbl <- x$tbl
-  
-  table.rows <- dplyr::count(tbl, name = "n") %>% dplyr::pull(n)
-  
-  column_list <- list(columns = lapply(col_schema(.tbl = tbl), as.list))
+  column_list <- list(columns = lapply(col_schema(.tbl = .tbl), as.list))
   
   for (i in seq_along(column_names)) {
     
@@ -152,13 +185,15 @@ create_metadata <- function(tbl = NULL,
       ),
       column_list
     )
-    
+
   # Create the metadata list object
   metadata <-
     list(
+      tbl = if (!read_fn_given) tbl,
       read_fn = read_fn,
       tbl_name = table.name,
       meta_label = label,
+      meta_snippets = list(),
       lang = lang,
       locale = locale,
       metadata = metadata_list
@@ -170,3 +205,4 @@ create_metadata <- function(tbl = NULL,
   
   metadata
 }
+

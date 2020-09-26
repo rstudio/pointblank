c1502e44e10ced37cc1f049dc811a691b8ea0a3f
Richard Iannone <riannone@me.com>
2020-09-25 23:54:29 -0400

    Add roxygen documentation

--- R/get_informant_report.R
+++ R/get_informant_report.R
@@ -10,6 +10,20 @@
 #' @param informant An informant object of class `ptblank_informant`.
 #' @param size The size of the display table, which can be either `"standard"`
 #'   (the default, with a width of 875px) or `"small"` (width of 575px).
+#' @param lang The language to use for the *information report* (a summary table
+#'   that provides the validation plan and the results from the interrogation.
+#'   By default, `NULL` will create English (`"en"`) text. Other options include
+#'   French (`"fr"`), German (`"de"`), Italian (`"it"`), Spanish (`"es"`),
+#'   Portuguese, (`"pt"`), Chinese (`"zh"`), and Russian (`"ru"`). This `lang`
+#'   option will override any previously set lang value (e.g., by the
+#'   [create_agent()] call).
+#' @param locale An optional locale ID to use for formatting values in the
+#'   *information report* summary table according the locale's rules. Examples
+#'   include `"en_US"` for English (United States) and `"fr_FR"` for French
+#'   (France); more simply, this can be a language identifier without a country
+#'   designation, like `"es"` for Spanish (Spain, same as `"es_ES"`). This
+#'   `locale` option will override any previously set locale value (e.g., by the
+#'   [create_agent()] call).
 #' 
 #' @return A **gt** table object.
 #' 

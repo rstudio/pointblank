bfd53530c6b24d5be4e04aaa1c9a29f6bcfd21be
Richard Iannone <riannone@me.com>
2020-10-03 00:00:39 -0400

    Update testthat test

---
+++ tests/testthat/test-scan_data.R
@@ -0,0 +1,57 @@
+test_that("Using `scan_data()` results in an HTML document", {
+  
+  skip_on_cran()
+  
+  scan_data_html <- 
+    scan_data(
+      tbl = small_table, 
+      sections = c("overview", "variables")
+    ) %>%
+    as.character()
+  
+  expect_equal(length(scan_data_html), 1)
+  
+  expect_true(
+    grepl(
+      "^<!doctype html>.*</body>.*?</html>$",
+      scan_data_html
+    )
+  )
+  
+  scan_data_sqlite_html <- 
+    scan_data(
+      tbl = small_table_sqlite(), 
+      sections = c("overview", "variables")
+    ) %>%
+    as.character()
+  
+  expect_equal(length(scan_data_sqlite_html), 1)
+  
+  expect_true(
+    grepl(
+      "^<!doctype html>.*</body>.*?</html>$",
+      scan_data_sqlite_html
+    )
+  )
+  
+  expect_error(
+    scan_data(
+      tbl = small_table,
+      sections = NULL
+    )
+  )
+  
+  expect_error(
+    scan_data(
+      tbl = small_table,
+      sections = c()
+    )
+  )
+  
+  expect_error(
+    scan_data(
+      tbl = small_table,
+      sections = "extra"
+    )
+  )
+})

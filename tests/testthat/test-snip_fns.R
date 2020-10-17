78c96a57e0c00bca87a0f725c531b1af6e3aaf33
Richard Iannone <riannone@me.com>
2020-10-17 17:30:50 -0400

    Create test-snip_fns.R

---
+++ tests/testthat/test-snip_fns.R
@@ -0,0 +1,60 @@
+
+run_snip <- function(snip_call, tbl) {
+  (snip_call %>%
+     rlang::f_rhs() %>%
+     rlang::eval_tidy())(tbl)
+}
+
+test_that("the `snip_list()` function works", {
+ 
+  expect_equal(
+    run_snip(snip_list(column = "f"), small_table),
+    "`\"high\"`, `\"low\"`, and `\"mid\"` "
+  )
+  expect_equal(
+    run_snip(snip_list(column = "f", limit = 5), small_table),
+    "`\"high\"`, `\"low\"`, and `\"mid\"` "
+  )
+  expect_equal(
+    run_snip(snip_list(column = "f", limit = Inf), small_table),
+    "`\"high\"`, `\"low\"`, and `\"mid\"` "
+  )
+  expect_equal(
+    run_snip(snip_list(column = "f", limit = 2), small_table),
+    "`\"high\"`, `\"low\"` (+1 more)"
+  )
+  expect_equal(
+    run_snip(snip_list(column = "f", limit = 1), small_table),
+    "`\"high\"` (+2 more)"
+  )
+  
+  expect_equal(
+    run_snip(snip_list(column = "c", limit = 5), small_table),
+    "`\"3\"`, `\"8\"`, `\"NA\"`, `\"7\"`, `\"4\"` (+2 more)"
+  )
+  expect_equal(
+    run_snip(snip_list(column = "c", limit = 2), small_table),
+    "`\"3\"`, `\"8\"` (+5 more)"
+  )
+  
+  expect_equal(
+    run_snip(snip_list(column = "e", limit = 5), small_table),
+    "`\"TRUE\"` and `\"FALSE\"`"
+  )
+  expect_equal(
+    run_snip(snip_list(column = "e", limit = 1), small_table),
+    "`\"TRUE\"` and `\"FALSE\"`"
+  )
+  
+  expect_equal(
+    run_snip(snip_list(column = "date_time", limit = 2), small_table),
+    "`\"2016-01-04 11:00:00\"`, `\"2016-01-04 00:32:00\"` (+10 more)"
+  )
+  
+  expect_equal(
+    run_snip(snip_list(column = "date", limit = 2), small_table),
+    "`\"2016-01-04\"`, `\"2016-01-05\"` (+9 more)"
+  )
+})
+
+

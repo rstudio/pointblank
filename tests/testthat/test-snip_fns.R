dd4e0f406482399b7a152cbab2fbada4d2524981
Richard Iannone <riannone@me.com>
2020-11-27 16:24:23 -0500

    Update test-snip_fns.R

--- tests/testthat/test-snip_fns.R
+++ tests/testthat/test-snip_fns.R
@@ -27,7 +27,6 @@ test_that("the `snip_list()` function works", {
     run_snip(snip_list(column = "f", limit = 1), small_table),
     "`\"high\"` (+2 more)"
   )
-  
   expect_equal(
     run_snip(snip_list(column = "c", limit = 5), small_table),
     "`3`, `8`, `NA`, `7`, `4` (+2 more)"
@@ -36,7 +35,6 @@ test_that("the `snip_list()` function works", {
     run_snip(snip_list(column = "c", limit = 2), small_table),
     "`3`, `8` (+5 more)"
   )
-  
   expect_equal(
     run_snip(snip_list(column = "e", limit = 5), small_table),
     "`TRUE` and `FALSE`"
@@ -53,12 +51,159 @@ test_that("the `snip_list()` function works", {
     run_snip(snip_list(column = "date", limit = 2), small_table),
     "`2016-01-04`, `2016-01-05` (+9 more)"
   )
-  
   expect_equal(
     run_snip(snip_list(column = "f", and_or = "or"), small_table),
     "`\"high\"`, `\"low\"`, or `\"mid\"`"
   )
-  
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, and_or = "and", lang = "de"),
+      small_table
+    ),
+    "`2016-01-04`, `2016-01-05`, `2016-01-06` (+8 mehr)"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = TRUE, quot_str = TRUE),
+      small_table
+    ),
+    "`\"2016-01-04\"`, `\"2016-01-05\"`, `\"2016-01-06\"` (+8 more)"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = TRUE, quot_str = FALSE),
+      small_table
+    ),
+    "`2016-01-04`, `2016-01-05`, `2016-01-06` (+8 more)"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = FALSE, quot_str = TRUE),
+      small_table
+    ),
+    "\"2016-01-04\", \"2016-01-05\", \"2016-01-06\" (+8 more)"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = FALSE, quot_str = FALSE),
+      small_table
+    ),
+    "2016-01-04, 2016-01-05, 2016-01-06 (+8 more)"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = TRUE, quot_str = TRUE),
+      small_table[2:3, ]
+    ),
+    "`\"2016-01-04\"` and `\"2016-01-05\"`"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = TRUE, quot_str = FALSE),
+      small_table[2:3, ]
+    ),
+    "`2016-01-04` and `2016-01-05`"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = FALSE, quot_str = TRUE),
+      small_table[2:3, ]
+    ),
+    "\"2016-01-04\" and \"2016-01-05\""
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = FALSE, quot_str = FALSE),
+      small_table[2:3, ]
+    ),
+    "2016-01-04 and 2016-01-05"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = TRUE, quot_str = TRUE, lang = "fr"),
+      small_table[2:3, ]
+    ),
+    "`\"2016-01-04\"` et `\"2016-01-05\"`"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = TRUE, quot_str = FALSE, lang = "fr"),
+      small_table[2:3, ]
+    ),
+    "`2016-01-04` et `2016-01-05`"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = FALSE, quot_str = TRUE, lang = "fr"),
+      small_table[2:3, ]
+    ),
+    "\"2016-01-04\" et \"2016-01-05\""
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = FALSE, quot_str = FALSE, lang = "fr"),
+      small_table[2:3, ]
+    ),
+    "2016-01-04 et 2016-01-05"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = TRUE, quot_str = TRUE, lang = "fr"),
+      small_table[2:4, ]
+    ),
+    "`\"2016-01-04\"`, `\"2016-01-05\"` et `\"2016-01-06\"`"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = TRUE, quot_str = FALSE, lang = "fr"),
+      small_table[2:4, ]
+    ),
+    "`2016-01-04`, `2016-01-05` et `2016-01-06`"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = FALSE, quot_str = TRUE, lang = "fr"),
+      small_table[2:4, ]
+    ),
+    "\"2016-01-04\", \"2016-01-05\" et \"2016-01-06\""
+  )
+  expect_equal(
+    run_snip(
+      snip_list(column = "date", limit = 3, as_code = FALSE, quot_str = FALSE, lang = "fr"),
+      small_table[2:4, ]
+    ),
+    "2016-01-04, 2016-01-05 et 2016-01-06"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(
+        column = "date", limit = 3, as_code = FALSE,
+        quot_str = FALSE, lang = "fr", oxford = TRUE
+      ),
+      small_table[2:4, ]
+    ),
+    "2016-01-04, 2016-01-05 et 2016-01-06"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(
+        column = "date", limit = 3, as_code = FALSE,
+        quot_str = FALSE, lang = "en", oxford = TRUE
+      ),
+      small_table[2:4, ]
+    ),
+    "2016-01-04, 2016-01-05, and 2016-01-06"
+  )
+  expect_equal(
+    run_snip(
+      snip_list(
+        column = "date", limit = 3, as_code = FALSE,
+        quot_str = FALSE, lang = "en", oxford = FALSE
+      ),
+      small_table[2:4, ]
+    ),
+    "2016-01-04, 2016-01-05 and 2016-01-06"
+  )
 })
 
 test_that("the `snip_lowest()` function works", {

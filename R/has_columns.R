814d000c5fc9fff034d701ecd0811cfe85e36685
Richard Iannone <riannone@me.com>
2020-12-01 22:43:29 -0500

    Add the `has_columns()` function

---
+++ R/has_columns.R
@@ -0,0 +1,44 @@
+#
+#                _         _    _      _                _    
+#               (_)       | |  | |    | |              | |   
+#  _ __    ___   _  _ __  | |_ | |__  | |  __ _  _ __  | | __
+# | '_ \  / _ \ | || '_ \ | __|| '_ \ | | / _` || '_ \ | |/ /
+# | |_) || (_) || || | | || |_ | |_) || || (_| || | | ||   < 
+# | .__/  \___/ |_||_| |_| \__||_.__/ |_| \__,_||_| |_||_|\_\
+# | |                                                        
+# |_|                                                        
+# 
+# This file is part of the 'rich-iannone/pointblank' package.
+# 
+# (c) Richard Iannone <riannone@me.com>
+# 
+# For full copyright and license information, please look at
+# https://rich-iannone.github.io/pointblank/LICENSE.html
+#
+
+
+#' Determine if columns exist in a table. 
+#' 
+#' @param x The table object.
+#' @param columns One or more column names that are to be checked for existence
+#' in the table `x`.
+#'   
+#' @return A length-1 logical vector.
+#'
+#' @export
+has_columns <- function(x, columns) {
+  
+  # Normalize the `columns` expression
+  if (inherits(columns, "quosures")) {
+    
+    columns <- 
+      vapply(
+        columns,
+        FUN.VALUE = character(1),
+        USE.NAMES = FALSE,
+        FUN = function(x) as.character(rlang::get_expr(x))
+      )
+  }
+  
+  all(columns %in% rlang::names2(x))
+}

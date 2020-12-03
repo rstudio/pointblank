9701460146f48ab26b1e8680436edd64fbbdc061
Richard Iannone <riannone@me.com>
2020-12-03 01:33:23 -0500

    Update function documentation

--- R/col_vals_increasing.R
+++ R/col_vals_increasing.R
@@ -86,7 +86,8 @@
 #' @param decreasing_tol An optional threshold value that allows for movement of
 #'   numerical values in the negative direction. By default this is `NULL` but
 #'   using a numerical value with set the absolute threshold of negative travel
-#'   allowed across numerical test units.
+#'   allowed across numerical test units. Note that setting a value here also
+#'   has the effect of setting `allow_stationary` to `TRUE`.
 #' 
 #' @family validation functions
 #' 

abf393af9a38fa86065f0df6346c3132550029d2
Richard Iannone <riannone@me.com>
2020-12-05 17:25:22 -0500

    Add docs and the `add_tz` arg

--- R/file_naming.R
+++ R/file_naming.R
@@ -16,13 +16,37 @@
 # https://rich-iannone.github.io/pointblank/LICENSE.html
 #
 
-#' Append a date string to a file name
+#' Put the current date into a file name
+#' 
+#' This function helps to affix the current date to a filename. This is useful
+#' when writing *agent* and/or *informant* objects to disk as part of a
+#' continuous process. The date can be in terms of UTC time or the local system
+#' time. The date can be affixed either to the end of the filename (before the
+#' file extension) or at the beginning with a customizable delimiter.
+#' 
+#' @param filename The filename to modify.
+#' @param position Where to place the formatted date. This could either be at
+#'   the `"end"` of the filename (the default) or at the `"start"`.
+#' @param format A [base::strptime()] format string for formatting the date. By
+#'   default, this is `"%Y-%m-%d"` which expresses the date according to the ISO
+#'   8601 standard (as `YYYY-MM-DD`). Refer to the documentation on
+#'   [base::strptime()] for conversion specifications if planning to use a
+#'   different format string.
+#' @param delimiter The delimiter characters to use for separating the date
+#'   string from the original file name.
+#' @param utc_time An option for whether to use the current UTC time to
+#'   establish the date (the default, with `TRUE`), or, use the system's local
+#'   time (`FALSE`).
+#' 
+#' @seealso The [affix_datetime()] function provides the same features except it
+#'   produces a date-time string by default.
+#' 
 #' @export
 affix_date <- function(filename,
                        position = c("end", "start"),
                        format = "%Y-%m-%d",
                        delimiter = "_",
-                       utc = TRUE) {
+                       utc_time = TRUE) {
   
   position <- match.arg(position)
   
@@ -31,17 +55,48 @@ affix_date <- function(filename,
     position = position,
     format = format,
     delimiter = delimiter,
-    utc = utc
+    utc_time = utc_time,
+    add_tz = FALSE
   )
 }
 
-#' Append a datetime string to a file name
+#' Put the current date-time into a file name
+#' 
+#' This function helps to affix the current date-time to a filename. This is
+#' useful when writing *agent* and/or *informant* objects to disk as part of a
+#' continuous process. The date-time string can be based on the current UTC time
+#' or the local system time. The date can be affixed either to the end of the
+#' filename (before the file extension) or at the beginning with a customizable
+#' delimiter.
+#' 
+#' @inheritParams affix_date
+#' @param position Where to place the formatted date-time. This could either be
+#'   at the `"end"` of the filename (the default) or at the `"start"`.
+#' @param format A [base::strptime()] format string for formatting the
+#'   date-time. By default, this is `"%Y-%m-%dT%H:%M:%S"` which expresses the
+#'   date according to the ISO 8601 standard. For example, if the current
+#'   date-time is 2020-12-04 13:11:23, the formatted string would become
+#'   `"2020-12-04T13:11:23"`. Refer to the documentation on [base::strptime()]
+#'   for conversion specifications if planning to use a different format string.
+#' @param delimiter The delimiter characters to use for separating the date-time
+#'   string from the original file name.
+#' @param utc_time An option for whether to use the current UTC time to
+#'   establish the date-time (the default, with `TRUE`), or, use the system's
+#'   local time (`FALSE`).
+#' @param add_tz Should the time zone (as an offset from UTC) be provided? If
+#'   `TRUE` (the default) then the UTC offset will be either provided as
+#'   `<time>Z` (if `utc_time = TRUE`) or `<time>(+/-)hhmm`.
+#'
+#' @seealso The [affix_date()] function provides the same features except it
+#'   produces a date string by default.
+#' 
 #' @export
 affix_datetime <- function(filename,
                            position = c("end", "start"),
                            format = "%Y-%m-%dT%H:%M:%S",
                            delimiter = "_",
-                           utc = TRUE) {
+                           utc_time = TRUE,
+                           add_tz = TRUE) {
   
   position <- match.arg(position)
   
@@ -50,7 +105,8 @@ affix_datetime <- function(filename,
     position = position,
     format = format,
     delimiter = delimiter,
-    utc = utc
+    utc_time = utc_time,
+    add_tz = add_tz
   )
 }
 
@@ -58,15 +114,17 @@ affix_time_to_filename <- function(filename,
                                    position,
                                    format,
                                    delimiter,
-                                   utc) {
+                                   utc_time,
+                                   add_tz) {
   
   curr_time <- Sys.time()
   
-  if (utc) {
+  if (utc_time) {
     attr(curr_time, "tzone") <- "UTC"
   }
-  
-  curr_time <- format(curr_time, format)
+
+  curr_time <- format(curr_time, ifelse(add_tz, paste0(format, "%z"), format))
+  curr_time <- gsub("\\+0000$", "Z", curr_time)
   
   if (position == "end") {
     
@@ -75,7 +133,7 @@ affix_time_to_filename <- function(filename,
         tools::file_path_sans_ext(filename),
         delimiter,
         curr_time,
-        ".",
+        ifelse(tools::file_ext(filename) != "", ".", ""),
         tools::file_ext(filename)
       )
     

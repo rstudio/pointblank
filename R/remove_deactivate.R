67774e88d20f16af6a2d0a93df66d714f079bffa
Richard Iannone <riannone@me.com>
2020-12-11 12:09:34 -0500

    Update remove_deactivate.R

--- R/remove_deactivate.R
+++ R/remove_deactivate.R
@@ -1,31 +1,78 @@
 #' Remove one or more of an *agent*'s validation steps
 #'
+#' @param agent An agent object of class `ptblank_agent`.
+#' @param i The validation step number, which is assigned to each validation
+#'   step in the order of definition. If `NULL` (the default) then step removal
+#'   won't occur by index.
+#'
+#' @return A `ptblank_agent` object. 
+#'
 #' @export
-remove_steps <- function(x,
+remove_steps <- function(agent,
                          i = NULL) {
   
   if (!is.null(i)) {
-    x$validation_set <- 
-      x$validation_set %>%
+    agent$validation_set <- 
+      agent$validation_set %>%
       dplyr::slice(-{{ i }})
   }
   
   # Renumber steps
-  x$validation_set$i <- 
-    as.integer(seq(from = 1, to = nrow(x$validation_set), by = 1))
+  agent$validation_set$i <- 
+    as.integer(seq(from = 1, to = nrow(agent$validation_set), by = 1))
   
   # Remove any data extracts
-  x$extracts <- NULL
+  agent$extracts <- NULL
   
-  x
+  agent
 }
 
 #' Deactivate one or more of an *agent*'s validation steps
 #'
+#' @param agent An agent object of class `ptblank_agent`.
+#' @param i The validation step number, which is assigned to each validation
+#'   step in the order of definition. If `NULL` (the default) then step
+#'   deactivation won't occur by index.
+#'
+#' @return A `ptblank_agent` object.
+#'
 #' @export
-deactivate_steps <- function(x,
+deactivate_steps <- function(agent,
                              i = NULL) {
   
+  if (!is.null(i)) {
+    agent$validation_set <- 
+      agent$validation_set %>%
+      dplyr::mutate(active = ifelse(i %in% {{ i }}, list(FALSE), active))
+  }
   
+  # Remove any data extracts
+  agent$extracts <- NULL
+  
+  agent
 }
 
+#' Activate one or more of an *agent*'s validation steps
+#'
+#' @param agent An agent object of class `ptblank_agent`.
+#' @param i The validation step number, which is assigned to each validation
+#'   step in the order of definition. If `NULL` (the default) then step
+#'   activation won't occur by index.
+#'
+#' @return A `ptblank_agent` object.
+#'
+#' @export
+activate_steps <- function(agent,
+                           i = NULL) {
+  
+  if (!is.null(i)) {
+    agent$validation_set <- 
+      agent$validation_set %>%
+      dplyr::mutate(active = ifelse(i %in% {{ i }}, list(TRUE), active))
+  }
+  
+  # Remove any data extracts
+  agent$extracts <- NULL
+  
+  agent
+}

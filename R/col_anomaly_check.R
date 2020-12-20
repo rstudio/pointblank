cf212fd56c7448c84d13c0b555b3c4ec77cf60d7
Richard Iannone <riannone@me.com>
2020-12-19 22:26:18 -0500

    Update col_anomaly_check.R

--- R/col_anomaly_check.R
+++ R/col_anomaly_check.R
@@ -18,6 +18,30 @@
 
 #' Do columns in the table contain anomalous values?
 #'
+#' The `col_anomaly_check()` validation function, the
+#' `expect_col_anomaly_check()` expectation function, and the
+#' `test_col_anomaly_check()` test function all check whether column values
+#' contain anomalous values in a time series. Because this family of functions
+#' only currently supports a time-series analysis for the detection of
+#' anomalies, the `x_column` must contain date-time values and the `y_column`
+#' must contain numeric values. The validation function can be used directly on
+#' a data table or with an *agent* object (technically, a `ptblank_agent`
+#' object) whereas the expectation and test functions can only be used with a
+#' data table. The types of data tables that can be used include data frames,
+#' tibbles, database tables (`tbl_dbi`), and Spark DataFrames (`tbl_spark`).
+#' Each validation step or expectation will operate over the number of test
+#' units that is equal to the number of rows in the table (after any
+#' `preconditions` have been applied).
+#'
+#' @inheritParams col_vals_gt
+#' @param x_column The column that contains the *x* values. This should be a
+#'   date-time column because, currently, the `col_anomaly_check()` function
+#'   only supports anomaly detection as part of a time-series analysis.
+#' @param y_column The column that contains the *y* values, which must be
+#'   numeric.
+#'
+#' @family validation functions
+#'
 #' @name col_anomaly_check
 NULL
 
@@ -69,9 +93,9 @@ col_anomaly_check <- function(x,
         assertion_type = "col_anomaly_check"
       )
   }
-  
+
   # Normalize any provided `step_id` value(s)
-  step_id <- normalize_step_id(step_id, columns, agent)
+  step_id <- normalize_step_id(step_id, columns = "column", agent)
   
   # Check `step_id` value(s) against all other `step_id`
   # values in earlier validation steps

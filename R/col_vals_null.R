49bdf6e47c2be9386eefc85efe82b54fe7083653
Richard Iannone <riannone@me.com>
2021-01-18 21:30:19 -0500

    Update col_vals_null.R

--- R/col_vals_null.R
+++ R/col_vals_null.R
@@ -164,6 +164,11 @@ col_vals_null <- function(x,
   
   values <- NULL
   
+  # Get `columns` as a label
+  columns_expr <- 
+    rlang::as_label(rlang::quo(!!enquo(columns))) %>%
+    gsub("^\"|\"$", "", .)
+  
   # Capture the `columns` expression
   columns <- rlang::enquo(columns)
   
@@ -199,6 +204,9 @@ col_vals_null <- function(x,
   # Normalize any provided `step_id` value(s)
   step_id <- normalize_step_id(step_id, columns, agent)
   
+  # Get the next step number for the `validation_set` tibble
+  i_o <- get_next_validation_set_row(agent)
+  
   # Check `step_id` value(s) against all other `step_id`
   # values in earlier validation steps
   check_step_id_duplicates(step_id, agent)
@@ -211,6 +219,8 @@ col_vals_null <- function(x,
       create_validation_step(
         agent = agent,
         assertion_type = "col_vals_null",
+        i_o = i_o,
+        columns_expr = columns_expr,
         column = columns[i],
         preconditions = preconditions,
         actions = covert_actions(actions, agent),

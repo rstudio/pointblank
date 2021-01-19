967dfc0f17961b9024dfcff12df012e5e3c953e5
Richard Iannone <riannone@me.com>
2021-01-18 21:29:37 -0500

    Update col_is_integer.R

--- R/col_is_integer.R
+++ R/col_is_integer.R
@@ -145,6 +145,11 @@ col_is_integer <- function(x,
   # Capture the `column` expression
   columns <- rlang::enquo(columns)
   
+  # Get `columns` as a label
+  columns_expr <- 
+    rlang::as_label(rlang::quo(!!enquo(columns))) %>%
+    gsub("^\"|\"$", "", .)
+  
   # Resolve the columns based on the expression
   columns <- resolve_columns(x = x, var_expr = columns, preconditions = NULL)
   
@@ -176,6 +181,9 @@ col_is_integer <- function(x,
   # Normalize any provided `step_id` value(s)
   step_id <- normalize_step_id(step_id, columns, agent)
   
+  # Get the next step number for the `validation_set` tibble
+  i_o <- get_next_validation_set_row(agent)
+  
   # Check `step_id` value(s) against all other `step_id`
   # values in earlier validation steps
   check_step_id_duplicates(step_id, agent)
@@ -188,6 +196,8 @@ col_is_integer <- function(x,
       create_validation_step(
         agent = agent,
         assertion_type = "col_is_integer",
+        i_o = i_o,
+        columns_expr = columns_expr,
         column = columns[i],
         preconditions = NULL,
         actions = covert_actions(actions, agent),

62e614c26bd163fe61536b20f215d32176ae0d21
Richard Iannone <riannone@me.com>
2021-01-18 21:29:54 -0500

    Update col_vals_expr.R

--- R/col_vals_expr.R
+++ R/col_vals_expr.R
@@ -223,6 +223,9 @@ col_vals_expr <- function(x,
   # Normalize any provided `step_id` value(s)
   step_id <- normalize_step_id(step_id, columns = "column", agent)
   
+  # Get the next step number for the `validation_set` tibble
+  i_o <- get_next_validation_set_row(agent)
+  
   # Check `step_id` value(s) against all other `step_id`
   # values in earlier validation steps
   check_step_id_duplicates(step_id, agent)
@@ -232,6 +235,8 @@ col_vals_expr <- function(x,
     create_validation_step(
       agent = agent,
       assertion_type = "col_vals_expr",
+      i_o = i_o,
+      columns_expr = NA_character_,
       column = NA_character_,
       values = expr,
       preconditions = preconditions,

f02e055fa055acabb102c06cff2269ca71b37fc0
Richard Iannone <riannone@me.com>
2021-01-18 21:29:32 -0500

    Update col_is_character.R

--- R/col_is_character.R
+++ R/col_is_character.R
@@ -141,6 +141,11 @@ col_is_character <- function(x,
   preconditions <- NULL
   values <- NULL
   
+  # Get `columns` as a label
+  columns_expr <- 
+    rlang::as_label(rlang::quo(!!enquo(columns))) %>%
+    gsub("^\"|\"$", "", .)
+  
   # Capture the `columns` expression
   columns <- rlang::enquo(columns)
   
@@ -175,6 +180,9 @@ col_is_character <- function(x,
   # Normalize any provided `step_id` value(s)
   step_id <- normalize_step_id(step_id, columns, agent)
   
+  # Get the next step number for the `validation_set` tibble
+  i_o <- get_next_validation_set_row(agent)
+  
   # Check `step_id` value(s) against all other `step_id`
   # values in earlier validation steps
   check_step_id_duplicates(step_id, agent)
@@ -187,6 +195,8 @@ col_is_character <- function(x,
       create_validation_step(
         agent = agent,
         assertion_type = "col_is_character",
+        i_o = i_o,
+        columns_expr = columns_expr,
         column = columns[i],
         preconditions = NULL,
         actions = covert_actions(actions, agent),

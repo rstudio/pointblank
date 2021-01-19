373ef4fcaa341ed25e53b69a77ba10e02703f55b
Richard Iannone <riannone@me.com>
2021-01-18 21:30:26 -0500

    Update conjointly.R

--- R/conjointly.R
+++ R/conjointly.R
@@ -259,6 +259,9 @@ conjointly <- function(x,
   # Normalize any provided `step_id` value(s)
   step_id <- normalize_step_id(step_id, columns = "column", agent)
   
+  # Get the next step number for the `validation_set` tibble
+  i_o <- get_next_validation_set_row(agent)
+  
   # Check `step_id` value(s) against all other `step_id`
   # values in earlier validation steps
   check_step_id_duplicates(step_id, agent)
@@ -268,6 +271,8 @@ conjointly <- function(x,
     create_validation_step(
       agent = agent,
       assertion_type = "conjointly",
+      i_o = i_o,
+      columns_expr = NULL,
       column = NULL,
       values = validation_formulas,
       na_pass = NULL,

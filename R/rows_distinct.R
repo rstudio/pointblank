c47bf34ffa6b5d5c550984f3f135d5d7ffbf5f56
Richard Iannone <riannone@me.com>
2021-01-18 21:30:28 -0500

    Update rows_distinct.R

--- R/rows_distinct.R
+++ R/rows_distinct.R
@@ -119,6 +119,11 @@ rows_distinct <- function(x,
                           brief = NULL,
                           active = TRUE) {
 
+  # Get `columns` as a label
+  columns_expr <- 
+    rlang::as_label(rlang::quo(!!enquo(columns))) %>%
+    gsub("^\"|\"$", "", .)
+  
   # Normalize the `columns` expression
   if (inherits(columns, "quosures")) {
     
@@ -176,6 +181,9 @@ rows_distinct <- function(x,
   # Normalize any provided `step_id` value(s)
   step_id <- normalize_step_id(step_id, columns = "column", agent)
   
+  # Get the next step number for the `validation_set` tibble
+  i_o <- get_next_validation_set_row(agent)
+  
   # Check `step_id` value(s) against all other `step_id`
   # values in earlier validation steps
   check_step_id_duplicates(step_id, agent)
@@ -185,6 +193,8 @@ rows_distinct <- function(x,
     create_validation_step(
       agent = agent,
       assertion_type = "rows_distinct",
+      i_o = i_o,
+      columns_expr = columns_expr,
       column = list(ifelse(is.null(columns), NA_character_, columns)),
       values = NULL,
       preconditions = preconditions,

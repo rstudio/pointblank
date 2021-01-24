d3d2b05463a274d63bcafef493e2d54fd0e2f196
Richard Iannone <riannone@me.com>
2021-01-24 01:33:24 -0500

    Update tests_label.R

--- tests/manual_tests/tests_label.R
+++ tests/manual_tests/tests_label.R
@@ -1,14 +1,21 @@
 library(pointblank)
 
-al <- action_levels(warn_at = 0.1, stop_at = 0.2)
-
 agent <-
-  small_table %>%
-  create_agent(actions = al) %>%
-  col_vals_gt(vars(date_time), vars(date), na_pass = TRUE) %>%
+  create_agent(
+    read_fn = ~ small_table,
+    actions = action_levels(warn_at = 0.1, stop_at = 0.2)
+  ) %>%
+  col_vals_gt(
+    vars(date_time), vars(date),
+    na_pass = TRUE
+  ) %>%
   col_vals_gt(
     vars(b), vars(g), na_pass = TRUE,
     label = "b > g"
+  ) %>%
+  col_is_character(
+    vars(b, f),
+    label = "Verifying character-type columns" 
   ) %>%
   rows_distinct(
     vars(d, e, f),

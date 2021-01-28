531d1aebc74bb7be1f4f44b47d050c6ee4121bb9
Richard Iannone <riannone@me.com>
2021-01-28 08:26:11 -0500

    Update tbl_from_db.R

--- R/tbl_from_db.R
+++ R/tbl_from_db.R
@@ -106,7 +106,7 @@
 #' # database... like this:
 #' # example_db_tbl <- 
 #' #   db_tbl(
-#' #     db = "<database_shortname>", 
+#' #     db = "<database_type_shortname>", 
 #' #     dbname = "<database_name>",
 #' #     table = "<table_name>",
 #' #     user = "<DB_USER_NAME>",

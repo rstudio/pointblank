c1140218d4d3f2ba023cb18c1890d2121f56477b
Richard Iannone <riannone@me.com>
2021-01-31 18:38:37 -0500

    Create col_vals_make_set.R

---
+++ R/col_vals_make_set.R
@@ -0,0 +1,340 @@
+#
+#                _         _    _      _                _    
+#               (_)       | |  | |    | |              | |   
+#  _ __    ___   _  _ __  | |_ | |__  | |  __ _  _ __  | | __
+# | '_ \  / _ \ | || '_ \ | __|| '_ \ | | / _` || '_ \ | |/ /
+# | |_) || (_) || || | | || |_ | |_) || || (_| || | | ||   < 
+# | .__/  \___/ |_||_| |_| \__||_.__/ |_| \__,_||_| |_||_|\_\
+# | |                                                        
+# |_|                                                        
+# 
+# This file is part of the 'rich-iannone/pointblank' package.
+# 
+# (c) Richard Iannone <riannone@me.com>
+# 
+# For full copyright and license information, please look at
+# https://rich-iannone.github.io/pointblank/LICENSE.html
+#
+
+
+#' Is a set of values entirely accounted for in a column of values?
+#'
+#' The `col_vals_make_set()` validation function, the
+#' `expect_col_vals_make_set()` expectation function, and the
+#' `test_col_vals_make_set()` test function all check whether `set` values are
+#' all seen at least once in a table column. A necessary criterion here is that
+#' no *additional* values (outside those definied in the `set`) should be seen
+#' (this requirement is relaxed in the [col_vals_include_subset()] validation
+#' function and in its expectation and test variants). The validation step
+#' function can be used directly on a data table or with an *agent* object
+#' (technically, a `ptblank_agent` object) whereas the expectation and test
+#' functions can only be used with a data table. The types of data tables that
+#' can be used include data frames, tibbles, database tables (`tbl_dbi`), and
+#' Spark DataFrames (`tbl_spark`). Each validation step or expectation will
+#' operate over the number of test units that is equal to the number of rows in
+#' the table (after any `preconditions` have been applied).
+#'
+#' If providing multiple column names, the result will be an expansion of
+#' validation steps to that number of column names (e.g., `vars(col_a, col_b)`
+#' will result in the entry of two validation steps). Aside from column names in
+#' quotes and in `vars()`, **tidyselect** helper functions are available for
+#' specifying columns. They are: `starts_with()`, `ends_with()`, `contains()`,
+#' `matches()`, and `everything()`.
+#' 
+#' Having table `preconditions` means **pointblank** will mutate the table just
+#' before interrogation. Such a table mutation is isolated in scope to the
+#' validation step(s) produced by the validation function call. Using
+#' **dplyr** code is suggested here since the statements can be translated to
+#' SQL if necessary. The code is most easily supplied as a one-sided **R**
+#' formula (using a leading `~`). In the formula representation, the `.` serves
+#' as the input data table to be transformed (e.g., 
+#' `~ . %>% dplyr::mutate(col_a = col_b + 10)`). Alternatively, a function could
+#' instead be supplied (e.g., 
+#' `function(x) dplyr::mutate(x, col_a = col_b + 10)`).
+#' 
+#' Often, we will want to specify `actions` for the validation. This argument,
+#' present in every validation function, takes a specially-crafted list
+#' object that is best produced by the [action_levels()] function. Read that
+#' function's documentation for the lowdown on how to create reactions to
+#' above-threshold failure levels in validation. The basic gist is that you'll
+#' want at least a single threshold level (specified as either the fraction of
+#' test units failed, or, an absolute value), often using the `warn_at`
+#' argument. This is especially true when `x` is a table object because,
+#' otherwise, nothing happens. For the `col_vals_*()`-type functions, using 
+#' `action_levels(warn_at = 0.25)` or `action_levels(stop_at = 0.25)` are good
+#' choices depending on the situation (the first produces a warning when a
+#' quarter of the total test units fails, the other `stop()`s at the same
+#' threshold level).
+#' 
+#' Want to describe this validation step in some detail? Keep in mind that this
+#' is only useful if `x` is an *agent*. If that's the case, `brief` the agent
+#' with some text that fits. Don't worry if you don't want to do it. The
+#' *autobrief* protocol is kicked in when `brief = NULL` and a simple brief will
+#' then be automatically generated.
+#'   
+#' @inheritParams col_vals_gt
+#' @param set A vector of numeric or string-based elements, where column values
+#'   found within this `set` will be considered as passing.
+#' @param complete A requirement to account for all column values in the
+#'   provided `set`. When this is `TRUE` all unique column values must be part
+#'   of the `set` (by default this is `FALSE`). If this mode is activated then
+#'   the number of test units reported will be the number of elements in the
+#'   `set`. This change is necessary since the perspective will be flipped.
+#' @param in_order A stringent requirement for enforcing the order of column
+#'   values to match that of the elements provided in the `set`. If `TRUE` (and
+#'   by default this is `FALSE`) the order of first-seen column values must
+#'   match the order given in the `set`. Furthermore, the column values need to
+#'   form contiguous blocks of each `set` element.
+#'   
+#' @return For the validation function, the return value is either a
+#'   `ptblank_agent` object or a table object (depending on whether an agent
+#'   object or a table was passed to `x`). The expectation function invisibly
+#'   returns its input but, in the context of testing data, the function is
+#'   called primarily for its potential side-effects (e.g., signaling failure).
+#'   The test function returns a logical value.
+#'   
+#' @examples
+#' # The `small_table` dataset in the
+#' # package will be used to validate that
+#' # column values are part of a given set
+#' 
+#' # A: Using an `agent` with validation
+#' #    functions and then `interrogate()`
+#' 
+#' # Validate that values in column `f`
+#' # are all part of the set of values
+#' # containing `low`, `mid`, and `high`
+#' agent <-
+#'   create_agent(small_table) %>%
+#'   col_vals_in_set(
+#'     vars(f), c("low", "mid", "high")
+#'   ) %>%
+#'   interrogate()
+#'   
+#' # Determine if this validation
+#' # had no failing test units (there
+#' # are 13 test units, one for each row)
+#' all_passed(agent)
+#' 
+#' # Calling `agent` in the console
+#' # prints the agent's report; but we
+#' # can get a `gt_tbl` object directly
+#' # with `get_agent_report(agent)`
+#' 
+#' # B: Using the validation function
+#' #    directly on the data (no `agent`)
+#' 
+#' # This way of using validation functions
+#' # acts as a data filter: data is passed
+#' # through but should `stop()` if there
+#' # is a single test unit failing; the
+#' # behavior of side effects can be
+#' # customized with the `actions` option
+#' small_table %>%
+#'   col_vals_in_set(
+#'     vars(f), c("low", "mid", "high")
+#'   ) %>%
+#'   dplyr::pull(f) %>%
+#'   unique()
+#'
+#' # C: Using the expectation function
+#' 
+#' # With the `expect_*()` form, we would
+#' # typically perform one validation at a
+#' # time; this is primarily used in
+#' # testthat tests
+#' expect_col_vals_in_set(
+#'   small_table,
+#'   vars(f), c("low", "mid", "high")
+#' )
+#' 
+#' # D: Using the test function
+#' 
+#' # With the `test_*()` form, we should
+#' # get a single logical value returned
+#' # to us
+#' small_table %>%
+#'   test_col_vals_in_set(
+#'     vars(f), c("low", "mid", "high")
+#'   )
+#' 
+#' @family validation functions
+#' @section Function ID:
+#' 2-x
+#' 
+#' @name col_vals_make_set
+NULL
+
+#' @rdname col_vals_make_set
+#' @import rlang
+#' @export
+col_vals_make_set <- function(x,
+                              columns,
+                              set,
+                              preconditions = NULL,
+                              actions = NULL,
+                              step_id = NULL,
+                              label = NULL,
+                              brief = NULL,
+                              active = TRUE) {
+  
+  # Get `columns` as a label
+  columns_expr <- 
+    rlang::as_label(rlang::quo(!!enquo(columns))) %>%
+    gsub("^\"|\"$", "", .)
+  
+  # Capture the `columns` expression
+  columns <- rlang::enquo(columns)
+  
+  # Resolve the columns based on the expression
+  columns <- resolve_columns(x = x, var_expr = columns, preconditions)
+  
+  if (is_a_table_object(x)) {
+    
+    secret_agent <-
+      create_agent(x, label = "::QUIET::") %>%
+      col_vals_make_set(
+        columns = columns,
+        set = set,
+        preconditions = preconditions,
+        label = label,
+        brief = brief,
+        actions = prime_actions(actions),
+        active = active
+      ) %>%
+      interrogate()
+    
+    return(x)
+  }
+  
+  agent <- x
+  
+  if (is.null(brief)) {
+    brief <- 
+      generate_autobriefs(
+        agent, columns, preconditions, values = set, "col_vals_make_set"
+      )
+  }
+  
+  # Normalize any provided `step_id` value(s)
+  step_id <- normalize_step_id(step_id, columns, agent)
+  
+  # Get the next step number for the `validation_set` tibble
+  i_o <- get_next_validation_set_row(agent)
+  
+  # Check `step_id` value(s) against all other `step_id`
+  # values in earlier validation steps
+  check_step_id_duplicates(step_id, agent)
+  
+  # Add one or more validation steps based on the
+  # length of the `columns` variable
+  for (i in seq(columns)) {
+    
+    agent <-
+      create_validation_step(
+        agent = agent,
+        assertion_type = "col_vals_make_set",
+        i_o = i_o,
+        columns_expr = columns_expr,
+        column = columns[i],
+        values = set,
+        preconditions = preconditions,
+        actions = covert_actions(actions, agent),
+        step_id = step_id[i],
+        label = label,
+        brief = brief[i],
+        active = active
+      )
+  }
+  
+  agent
+}
+
+
+#' @rdname col_vals_make_set
+#' @import rlang
+#' @export
+expect_col_vals_make_set <- function(object,
+                                     columns,
+                                     set,
+                                     preconditions = NULL,
+                                     threshold = 1) {
+  
+  fn_name <- "expect_col_vals_make_set"
+  
+  vs <- 
+    create_agent(tbl = object, label = "::QUIET::") %>%
+    col_vals_make_set(
+      columns = {{ columns }},
+      set = {{ set }}, 
+      preconditions = {{ preconditions }},
+      actions = action_levels(notify_at = threshold)
+    ) %>%
+    interrogate() %>%
+    .$validation_set
+  
+  x <- vs$notify %>% all()
+  
+  threshold_type <- get_threshold_type(threshold = threshold)
+  
+  if (threshold_type == "proportional") {
+    failed_amount <- vs$f_failed
+  } else {
+    failed_amount <- vs$n_failed
+  }
+  
+  if (inherits(vs$capture_stack[[1]]$warning, "simpleWarning")) {
+    warning(conditionMessage(vs$capture_stack[[1]]$warning))
+  }
+  if (inherits(vs$capture_stack[[1]]$error, "simpleError")) {
+    stop(conditionMessage(vs$capture_stack[[1]]$error))
+  }
+  
+  act <- testthat::quasi_label(enquo(x), arg = "object")
+  
+  column_text <- prep_column_text(vs$column[[1]])
+  values_text <- prep_values_text(values = vs$values, limit = 3, lang = "en")
+  
+  testthat::expect(
+    ok = identical(!as.vector(act$val), TRUE),
+    failure_message = glue::glue(
+      failure_message_gluestring(
+        fn_name = fn_name, lang = "en"
+      )
+    )
+  )
+  
+  act$val <- object
+  
+  invisible(act$val)
+}
+
+#' @rdname col_vals_make_set
+#' @import rlang
+#' @export
+test_col_vals_make_set <- function(object,
+                                   columns,
+                                   set,
+                                   preconditions = NULL,
+                                   threshold = 1) {
+  
+  vs <- 
+    create_agent(tbl = object, label = "::QUIET::") %>%
+    col_vals_make_set(
+      columns = {{ columns }},
+      set = {{ set }}, 
+      preconditions = {{ preconditions }},
+      actions = action_levels(notify_at = threshold)
+    ) %>%
+    interrogate() %>%
+    .$validation_set
+  
+  if (inherits(vs$capture_stack[[1]]$warning, "simpleWarning")) {
+    warning(conditionMessage(vs$capture_stack[[1]]$warning))
+  }
+  if (inherits(vs$capture_stack[[1]]$error, "simpleError")) {
+    stop(conditionMessage(vs$capture_stack[[1]]$error))
+  }
+  
+  all(!vs$notify)
+}

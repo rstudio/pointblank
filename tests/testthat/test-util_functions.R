1d3b9720c391f381df3435d71e5ee951bbc465eb
Richard Iannone <riannone@me.com>
2021-01-31 22:39:15 -0500

    Update test-util_functions.R

--- tests/testthat/test-util_functions.R
+++ tests/testthat/test-util_functions.R
@@ -325,7 +325,7 @@ test_that("Utility functions won't fail us", {
       "compare_expectation_text", "compare_failure_text", 
       "in_set_expectation_text", "in_set_failure_text",
       "make_set_expectation_text", "make_set_failure_text",
-      "make_subset_expectation_text", "make_subset_failure_text",
+      "make_subset_expectation_text", "make_susbset_failure_text",
       "not_in_set_expectation_text", "not_in_set_failure_text", 
       "between_expectation_text", "between_failure_text",
       "not_between_expectation_text", "not_between_failure_text",

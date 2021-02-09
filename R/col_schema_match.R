c808f7104e0b76f20629b1f4ee86d50ac890de26
Richard Iannone <riannone@me.com>
2021-02-08 22:37:49 -0500

    Update col_schema_match.R

--- R/col_schema_match.R
+++ R/col_schema_match.R
@@ -68,6 +68,54 @@
 #' *autobrief* protocol is kicked in when `brief = NULL` and a simple brief will
 #' then be automatically generated.
 #' 
+#' @section YAML:
+#' A **pointblank** agent can be written to YAML with [yaml_write()] and the
+#' resulting YAML can be used to regenerate an agent (with [yaml_read_agent()])
+#' or interrogate the target table (via [yaml_agent_interrogate()]). When
+#' `col_schema_match()` is represented in YAML (under the top-level `steps` key
+#' as a list member), the syntax closely follows the signature of the validation
+#' function. Here is an example of how a complex call of `col_schema_match()` as
+#' a validation step is expressed in R code and in the corresponding YAML
+#' representation.
+#' 
+#' ```
+#' # R statement
+#' agent %>% 
+#'   col_schema_match(
+#'     schema = col_schema(
+#'       a = "integer",
+#'       b = "character"
+#'     ), 
+#'     complete = FALSE,
+#'     in_order = FALSE,
+#'     is_exact = FALSE,
+#'     actions = action_levels(stop_at = 1),
+#'     label = "The `col_schema_match()` step.",
+#'     active = FALSE
+#'   )
+#' 
+#' # YAML representation
+#' steps:
+#' - col_schema_match:
+#'     schema:
+#'       a: integer
+#'       b: character
+#'     complete: false
+#'     in_order: false
+#'     is_exact: false
+#'     actions:
+#'       stop_count: 1.0
+#'     label: The `col_schema_match()` step.
+#'     active: false
+#' ```
+#' 
+#' In practice, both of these will often be shorter as only the `schema`
+#' argument requires a value. Arguments with default values won't be written to
+#' YAML when using [yaml_write()] (though it is acceptable to include them with
+#' their default when generating the YAML by other means). It is also possible
+#' to preview the transformation of an agent to YAML without any writing to disk
+#' by using the [yaml_agent_string()] function.
+#' 
 #' @inheritParams col_vals_gt
 #' @param schema A table schema of type `col_schema` which can be generated
 #'   using the [col_schema()] function.

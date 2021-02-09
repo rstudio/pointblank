9f820f3022e6255d6a125cd6756210e2e7f9897a
Richard Iannone <riannone@me.com>
2021-02-08 23:22:32 -0500

    Update col_is_character.R

--- R/col_is_character.R
+++ R/col_is_character.R
@@ -64,10 +64,10 @@
 #' A **pointblank** agent can be written to YAML with [yaml_write()] and the
 #' resulting YAML can be used to regenerate an agent (with [yaml_read_agent()])
 #' or interrogate the target table (via [yaml_agent_interrogate()]). When
-#' `col_is_character()` is represented in YAML (under the top-level `steps` key as a
-#' list member), the syntax closely follows the signature of the validation
-#' function. Here is an example of how a complex call of `col_is_character()` as a
-#' validation step is expressed in R code and in the corresponding YAML
+#' `col_is_character()` is represented in YAML (under the top-level `steps` key
+#' as a list member), the syntax closely follows the signature of the validation
+#' function. Here is an example of how a complex call of `col_is_character()` as
+#' a validation step is expressed in R code and in the corresponding YAML
 #' representation.
 #' 
 #' ```

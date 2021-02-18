6a86e26418037c850316179dd4991d5696726d67
Richard Iannone <riannone@me.com>
2021-02-18 12:42:19 -0500

    Update test-file_naming.R

--- tests/testthat/test-file_naming.R
+++ tests/testthat/test-file_naming.R
@@ -219,56 +219,56 @@ test_that("affixing a datetime to a filename works well", {
       filename = file_name_ext, position = "end",
       delimiter = "--", utc_time = TRUE, add_tz = TRUE
     ),
-    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z.txt"
+    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}Z.txt"
   )
   expect_match(
     affix_datetime(
       filename = file_name_ext, position = "end",
       delimiter = "--", utc_time = FALSE, add_tz = TRUE
     ),
-    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}((+|-)[0-9]{4}|Z).txt"
+    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}((+|-)[0-9]{4}|Z).txt"
   )
   expect_match(
     affix_datetime(
       filename = file_name_ext, position = "start",
       delimiter = "--", utc_time = TRUE, add_tz = TRUE
     ),
-    "20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z--the_file.txt"
+    "20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}Z--the_file.txt"
   )
   expect_match(
     affix_datetime(
       filename = file_name_ext, position = "start",
       delimiter = "--", utc_time = FALSE, add_tz = TRUE
     ),
-    "20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}((+|-)[0-9]{4}|Z)--the_file.txt"
+    "20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}((+|-)[0-9]{4}|Z)--the_file.txt"
   )
   expect_match(
     affix_datetime(
       filename = file_name, position = "end",
       delimiter = "--", utc_time = TRUE, add_tz = TRUE
     ),
-    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z"
+    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}Z"
   )
   expect_match(
     affix_datetime(
       filename = file_name, position = "end",
       delimiter = "--", utc_time = FALSE, add_tz = TRUE
     ),
-    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}((+|-)[0-9]{4}|Z)"
+    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}((+|-)[0-9]{4}|Z)"
   )
   expect_match(
     affix_datetime(
       filename = file_name, position = "start",
       delimiter = "--", utc_time = TRUE, add_tz = TRUE
     ),
-    "20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z--the_file"
+    "20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}Z--the_file"
   )
   expect_match(
     affix_datetime(
       filename = file_name, position = "start",
       delimiter = "--", utc_time = FALSE, add_tz = TRUE
     ),
-    "20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}((+|-)[0-9]{4}|Z)--the_file"
+    "20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}((+|-)[0-9]{4}|Z)--the_file"
   )
   expect_match(
     affix_datetime(
@@ -331,55 +331,55 @@ test_that("affixing a datetime to a filename works well", {
       filename = file_name_ext, position = "end",
       delimiter = "--", utc_time = TRUE, add_tz = FALSE
     ),
-    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.txt"
+    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}.txt"
   )
   expect_match(
     affix_datetime(
       filename = file_name_ext, position = "end",
       delimiter = "--", utc_time = FALSE, add_tz = FALSE
     ),
-    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.txt"
+    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}.txt"
   )
   expect_match(
     affix_datetime(
       filename = file_name_ext, position = "start",
       delimiter = "--", utc_time = TRUE, add_tz = FALSE
     ),
-    "20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}--the_file.txt"
+    "20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}--the_file.txt"
   )
   expect_match(
     affix_datetime(
       filename = file_name_ext, position = "start",
       delimiter = "--", utc_time = FALSE, add_tz = FALSE
     ),
-    "20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}--the_file.txt"
+    "20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}--the_file.txt"
   )
   expect_match(
     affix_datetime(
       filename = file_name, position = "end",
       delimiter = "--", utc_time = TRUE, add_tz = FALSE
     ),
-    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}"
+    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}"
   )
   expect_match(
     affix_datetime(
       filename = file_name, position = "end",
       delimiter = "--", utc_time = FALSE, add_tz = FALSE
     ),
-    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}"
+    "the_file--20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}"
   )
   expect_match(
     affix_datetime(
       filename = file_name, position = "start",
       delimiter = "--", utc_time = TRUE, add_tz = FALSE
     ),
-    "20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}--the_file"
+    "20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}--the_file"
   )
   expect_match(
     affix_datetime(
       filename = file_name, position = "start",
       delimiter = "--", utc_time = FALSE, add_tz = FALSE
     ),
-    "20[0-9]{2}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}--the_file"
+    "20[0-9]{2}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}--the_file"
   )
 })

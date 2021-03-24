f5486d34c1d6ed85206b9f59d0afc2bc8f635aba
Richard Iannone <riannone@me.com>
2021-03-23 22:46:56 -0400

    Update zz-process_datasets.R

--- data-raw/zz-process_datasets.R
+++ data-raw/zz-process_datasets.R
@@ -1,10 +1,11 @@
 library(usethis)
 
 source("data-raw/01-small_table.R")
+source("data-raw/03-countries_subdivisions_currencies.R")
 
 # Create external datasets
 
 usethis::use_data(
-  small_table,
+  small_table, countries, subdivisions, currencies,
   internal = FALSE, overwrite = TRUE
 )

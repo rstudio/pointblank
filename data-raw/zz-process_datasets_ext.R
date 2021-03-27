18cb9f1dade4894ab6a8383815ca259b55d83c7d
Richard Iannone <riannone@me.com>
2021-03-27 10:39:51 -0400

    Split `zz-process_datasets.R` into two files

--- data-raw/zz-process_datasets_ext.R
+++ data-raw/zz-process_datasets_ext.R
@@ -1,11 +1,10 @@
 library(usethis)
 
 source("data-raw/01-small_table.R")
-source("data-raw/03-countries_subdivisions_currencies.R")
 
 # Create external datasets
 
 usethis::use_data(
-  small_table, countries, subdivisions, subd_list_main, currencies, 
+  small_table,
   internal = FALSE, overwrite = TRUE
 )

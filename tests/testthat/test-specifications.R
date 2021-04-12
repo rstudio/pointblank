e0c0169116006562f513dc8062359b3ea48f2a07
Richard Iannone <riannone@me.com>
2021-04-11 20:44:01 -0400

    Create test-specifications.R

---
+++ tests/testthat/test-specifications.R
@@ -0,0 +1,501 @@
+iban_valid <- 
+  list(
+    AL = c(
+      "AL86751639367318444714198669",
+      "AL89515635252277023782748302",
+      "AL39153296222641598198140883",
+      "AL47907501989147671525950076",
+      "AL55398719849655505231753964"
+    ),
+    AD = c(
+      "AD2531377125214715353449",
+      "AD9764782778017799549345",
+      "AD4079739934060166934190",
+      "AD3210446914824799260335",
+      "AD1781438353588817727122"
+    ),
+    AT = c(
+      "AT582774098454337653",
+      "AT220332087576467472",
+      "AT328650112318219886",
+      "AT193357281080332578",
+      "AT535755326448639816"
+    ),
+    BE = c(
+      "BE16517682243567",
+      "BE46943937718104",
+      "BE75270187592710",
+      "BE58465045170210",
+      "BE49149522496291"
+    ),
+    BA = c(
+      "BA534130469841865537",
+      "BA515388988295860588",
+      "BA182655808222815318",
+      "BA105531662061034080",
+      "BA198940842595891985"
+    ),
+    BG = c(
+      "BG08NXYF73308507056085",
+      "BG22OOPG05631394112384",
+      "BG30XCMJ43923350257238",
+      "BG66ZKSV96204746173581",
+      "BG62TOZJ59790808155256"
+    ),
+    BR = "BR2301137514445981860933159P9",
+    HR = c(
+      "HR9118658081801951861",
+      "HR7093391174762888131",
+      "HR6824554207539191367",
+      "HR7069604594001692768",
+      "HR4270163171014341308"
+    ),
+    CY = c(
+      "CY48590776872388131193566182",
+      "CY57511427289148815512463528",
+      "CY10469623011747193079305488",
+      "CY86826022479357551507194222",
+      "CY65139035183710553510799793"
+    ),
+    CZ = c(
+      "CZ3740083988228867633610",
+      "CZ4923390395798905071131",
+      "CZ3697747307026104738861",
+      "CZ5061223246730267064210",
+      "CZ3638452806288471256750"
+    ),
+    DK = c(
+      "DK8387188644726815",
+      "DK3068706775436067",
+      "DK0697654450063121",
+      "DK1099979861456738",
+      "DK6988299842527195"
+    ),
+    EE = c(
+      "EE416702219844182076",
+      "EE816382882633746409",
+      "EE035815981173988529",
+      "EE150595733987082728",
+      "EE605409451030627522"
+    ),
+    FO = c(
+      "FO1593707486505366",
+      "FO5006907768039839",
+      "FO9378537341306148",
+      "FO4068759083981752",
+      "FO0905894981715676"
+    ),
+    FI = c(
+      "FI8709549333658747",
+      "FI1518471099159022",
+      "FI0589161476500751",
+      "FI8433982173935580",
+      "FI7954405150189238"
+    ),
+    FR = c(
+      "FR4197944644738285027717680",
+      "FR9476231310567227640169067",
+      "FR6888474339535547405026268",
+      "FR3007344050937354660134854",
+      "FR8547764510959591053030050"
+    ),
+    PF = c(
+      "PF8169352568136984283973639",
+      "PF1021725003919279759512045",
+      "PF9067348885442846702112667",
+      "PF2110055440192380776287331",
+      "PF4462138104308037716665461"
+    ),
+    TF = c(
+      "TF7369356610212036082878842",
+      "TF1699071511365858327828309",
+      "TF9287657455706592772258930",
+      "TF6983084059527026532259346",
+      "TF6320136548014311655407753"
+    ),
+    DE = c(
+      "DE06495352657836424132",
+      "DE09121688720378475751",
+      "DE88516399675378845887",
+      "DE42187384985716759572",
+      "DE04399340668928275395"
+    ),
+    GI = c(
+      "GI84YQVE742322843673354",
+      "GI50TRZE832226672231136",
+      "GI96DQBV940980418323607",
+      "GI12MTEJ300936244995281",
+      "GI50NKEA869461619367593"
+    ),
+    GE = c(
+      "GE27JX0363248286073573",
+      "GE95PE2036699405919650",
+      "GE86WI1894058889642409",
+      "GE50ZK0956993434292828",
+      "GE60VX8276008964044900"
+    ),
+    GR = c(
+      "GR8206922880502260960449182",
+      "GR0708312360607104632724143",
+      "GR3019549951345337224826989",
+      "GR1850333105485787816165828",
+      "GR3328425960116597801941217"
+    ),
+    GL = c(
+      "GL3357098231928641",
+      "GL7672801402871438",
+      "GL8576657033000228",
+      "GL7533425696727320",
+      "GL3145184332080211"
+    ),
+    HU = c(
+      "HU53165228954563006441576439",
+      "HU61442178338678431742505774",
+      "HU64774233934011029174507108",
+      "HU79689064758089616754511009",
+      "HU60873329200412252359645504"
+    ),
+    IS = c(
+      "IS098954934397185843549690",
+      "IS367580035808668402924142",
+      "IS179684724271989278617740",
+      "IS846240236716627404368872",
+      "IS521951362135843206164749"
+    ),
+    IE = c(
+      "IE49BENI35972029450251",
+      "IE37OGUG54280567980573",
+      "IE43DCUZ91231044680662",
+      "IE15AAKO11199097933967",
+      "IE77PIHS49175290558839"
+    ),
+    IL = c(
+      "IL454322198734138455151",
+      "IL839799606658366056087",
+      "IL038569613554041996868",
+      "IL813919026399312117293",
+      "IL654645042217944600527"
+    ),
+    IT = c(
+      "IT85M5508898545109326040966",
+      "IT52G4674641537627600627273",
+      "IT54K9621595703270001697912",
+      "IT02F7240326523239438656917",
+      "IT75F6444007486118207984348"
+    ),
+    LV = c(
+      "LV85QMUO0600628590552",
+      "LV06FYUQ8115346663782",
+      "LV05OXNQ0057259369767",
+      "LV22XGHZ6356462010762",
+      "LV27LLIK8896580861638"
+    ),
+    LB = c(
+      "LB82586807590631203627574587",
+      "LB33405622563828555835796785",
+      "LB04715710805951055803616185",
+      "LB61420797023022242826619522",
+      "LB67864629408749872547678117"
+    ),
+    LI = c(
+      "LI4091221689235313176",
+      "LI7615336074136062084",
+      "LI3727301137968672218",
+      "LI3551318446915634574",
+      "LI4705272204109186337"
+    ),
+    LT = c(
+      "LT369967216439021801",
+      "LT937444321684957069",
+      "LT424971109068400772",
+      "LT566572547785167976",
+      "LT344806290778854389"
+    ),
+    LU = c(
+      "LU292357816107922497",
+      "LU184883493877746720",
+      "LU850789684146586224",
+      "LU365548629753608759",
+      "LU954093702688849179"
+    ),
+    MK = c(
+      "MK72125600332161582",
+      "MK11033425562019483",
+      "MK28337919411434742",
+      "MK22345789402386151",
+      "MK82644233974800672"
+    ),
+    MT = c(
+      "MT97ATVB58306859106316239974172",
+      "MT74VCFO64435204415027820548935",
+      "MT35ITGC82712389863518284695353",
+      "MT68DQVR03392795978045273628339",
+      "MT29SUTJ80635803074721583494800"
+    ),
+    MU = c(
+      "MU51SJFJ6257989899845328236GLS",
+      "MU61KWPF5078030841109086598WUO",
+      "MU53JZOY7025842098740945151ZDV",
+      "MU33GHPP0512367410476524003SGD",
+      "MU47IDNI5979337138037202943JSF"
+    ),
+    YT = c(
+      "YT2364450161155207655772895",
+      "YT5387838908762423789181088",
+      "YT5732176546694896615831590",
+      "YT9824037454721994164038623",
+      "YT2841514046462334743686132"
+    ),
+    MC = c(
+      "MC7310021462396304214555821",
+      "MC7426943447019580313912629",
+      "MC5828214954205338889828744",
+      "MC5492313829455176982975920",
+      "MC9281452662355894512310924"
+    ),
+    ME = c(
+      "ME60043032533135219382",
+      "ME13188638660227646081",
+      "ME82608318996043837340",
+      "ME15121909794401990976",
+      "ME76417412116089156198"
+    ),
+    NL = c(
+      "NL23CGPQ0251595242",
+      "NL21FPBW0870850199",
+      "NL15TVWK0331902885",
+      "NL13RTEF0518590011",
+      "NL40SGFW1252215983"
+    ),
+    NC = c(
+      "NC9532788614647022310269396",
+      "NC2053292379717332255189037",
+      "NC9105701404726570049169877",
+      "NC1021801619496974025318651",
+      "NC1729434258559239166499130"
+    ),
+    NO = c(
+      "NO5384279272034",
+      "NO9739077211102",
+      "NO7009234138626",
+      "NO4448361377130",
+      "NO7962522169141"
+    ),
+    PL = c(
+      "PL58515427093787930748060666",
+      "PL22980511341176988398762666",
+      "PL08239642036391620641611736",
+      "PL67449258602191338152126294",
+      "PL82771306277364889467742211"
+    ),
+    PT = c(
+      "PT12014625392693045083592",
+      "PT80898047569635824751698",
+      "PT12065330847682220414039",
+      "PT98681708278396096913836",
+      "PT49242951581988705914025"
+    ),
+    RO = c(
+      "RO14JLFB9551925334163469",
+      "RO81QBBE5290470985636122",
+      "RO11VYHO3215271561449480",
+      "RO57EAOP2023783320803714",
+      "RO21HNFU2813681045796465"
+    ),
+    PM = c(
+      "PM8059411251360674293481450",
+      "PM5203212193960732379060042",
+      "PM7466602890486264340672969",
+      "PM6367055534424386034425612",
+      "PM2060260873302215070303208"
+    ),
+    SM = c(
+      "SM70N8724751165335491824812",
+      "SM78X1135489836211118891839",
+      "SM97M4888143036388138800185",
+      "SM72C9584723533916792029340",
+      "SM90M9981908196491432695525"
+    ),
+    SA = c(
+      "SA5591720552379162070001",
+      "SA0545544944406431392597",
+      "SA1667630781004847967711",
+      "SA2589813740329129166910",
+      "SA7081962486570441251637"
+    ),
+    RS = c(
+      "RS85033307149788542871",
+      "RS52665698845368481211",
+      "RS82691654340096587307",
+      "RS88844660406878687897",
+      "RS55472917853273859291"
+    ),
+    SK = c(
+      "SK4167111421162529673536",
+      "SK4589732621505695319336",
+      "SK4492457066924445710519",
+      "SK9190300791649333346556",
+      "SK6835978956449243145407"
+    ),
+    SI = c("SI26085198624502816",
+           "SI85363467889027196",
+           "SI93016808632808860",
+           "SI45000543512611896",
+           "SI14647150971707561"
+    ),
+    ES = c(
+      "ES2364265841767173822054",
+      "ES5577644480024527929849",
+      "ES7502766977729557202723",
+      "ES3282395478259622275430",
+      "ES9034258324029250165663"
+    ),
+    SE = c(
+      "SE3159169406714737443256",
+      "SE2636432381651868407029",
+      "SE3280552515152942260664",
+      "SE7905464316022155413548",
+      "SE8953084170161031273426"
+    ),
+    CH = c(
+      "CH1987364322975299818",
+      "CH4269286867620396437",
+      "CH2296292579429731980",
+      "CH6518929919723772608",
+      "CH9093021641139942126"
+    ),
+    TN = c(
+      "TN9670288139885457943736",
+      "TN8738524364626879391407",
+      "TN7275949269046889239714",
+      "TN4006837077003057397517",
+      "TN8683931110271287238460"
+    ),
+    TR = c(
+      "TR493318798613751080384953",
+      "TR314256533622834177853745",
+      "TR080572402207758013538147",
+      "TR489116538521358266645727",
+      "TR795585070398853758044433"
+    ),
+    GB = c(
+      "GB39MUJS50172570996370",
+      "GB14SIPV86193224493527",
+      "GB55ZAFY89851748597528",
+      "GB22KVUM18028477988401",
+      "GB26JAYK66540091518150"
+    ),
+    WF = c(
+      "WF5664222423044623595985593",
+      "WF6125565335534356679570561",
+      "WF4041383920092945092359281",
+      "WF0721812715683400832634716",
+      "WF6876262234744814330049391"
+    )
+  )
+
+
+vin_valid <-
+  c(
+    "1FTEW1E41KKD70581",
+    "ZARBAAB46LM355009",
+    "JTEBH3FJ60K093139",
+    "1HD1FS4178Y631180",
+    "WMWXP7C5XF2A33854",
+    "2CNFLNEY6A6356759",
+    "1GTR2VE78CZ109794",
+    "WDBRF64J63F357663",
+    "WAUKMAF47JA138978",
+    "1FAHP2FW8BG154879",
+    "4T1BE32K46U663238",
+    "SAJWA82BX9SH30556",
+    "JTMBFREV80D193233",
+    "1XP5DB9XX1N551710",
+    "JTMZD33V585094329",
+    "1J4GW48S42C322033",
+    "KM8K33A56JU088201",
+    "1M8GDM9AXKP042788",
+    "1HGBH41JXMN109186",
+    "WAUZZZF49HA036784"
+  )
+
+vin_invalid <-
+  c(
+    "7A8GK4M0706100372",
+    "WVWZZZ1KZ7U022191",
+    "JHLRE48507C210824",
+    "JM0BK10F100120307",
+    "KL3CD266JDB091630"
+  )
+
+test_that("IBAN numbers can be successfully validated", {
+  
+  # Check all IBAN numbers for weak validity (i.e, validity not
+  # based on country-specific features)
+  expect_true(all(check_iban(unname(unlist(iban_valid)))))
+  
+  # Check IBAN numbers from different countries
+  expect_true(all(check_iban(iban_valid[["AL"]], country = "AL")))
+  expect_true(all(check_iban(iban_valid[["AD"]], country = "AD")))
+  expect_true(all(check_iban(iban_valid[["AT"]], country = "AT")))
+  expect_true(all(check_iban(iban_valid[["BE"]], country = "BE")))
+  expect_true(all(check_iban(iban_valid[["BA"]], country = "BA")))
+  expect_true(all(check_iban(iban_valid[["BG"]], country = "BG")))
+  expect_true(all(check_iban(iban_valid[["BR"]], country = "BR")))
+  expect_true(all(check_iban(iban_valid[["HR"]], country = "HR")))
+  expect_true(all(check_iban(iban_valid[["CY"]], country = "CY")))
+  expect_true(all(check_iban(iban_valid[["CZ"]], country = "CZ")))
+  expect_true(all(check_iban(iban_valid[["DK"]], country = "DK")))
+  expect_true(all(check_iban(iban_valid[["EE"]], country = "EE")))
+  expect_true(all(check_iban(iban_valid[["FO"]], country = "FO")))
+  expect_true(all(check_iban(iban_valid[["FI"]], country = "FI")))
+  expect_true(all(check_iban(iban_valid[["FR"]], country = "FR")))
+  expect_true(all(check_iban(iban_valid[["PF"]], country = "PF")))
+  expect_true(all(check_iban(iban_valid[["TF"]], country = "TF")))
+  expect_true(all(check_iban(iban_valid[["DE"]], country = "DE")))
+  expect_true(all(check_iban(iban_valid[["GI"]], country = "GI")))
+  expect_true(all(check_iban(iban_valid[["GE"]], country = "GE")))
+  expect_true(all(check_iban(iban_valid[["GR"]], country = "GR")))
+  expect_true(all(check_iban(iban_valid[["GL"]], country = "GL")))
+  expect_true(all(check_iban(iban_valid[["HU"]], country = "HU")))
+  expect_true(all(check_iban(iban_valid[["IS"]], country = "IS")))
+  expect_true(all(check_iban(iban_valid[["IE"]], country = "IE")))
+  expect_true(all(check_iban(iban_valid[["IL"]], country = "IL")))
+  expect_true(all(check_iban(iban_valid[["IT"]], country = "IT")))
+  expect_true(all(check_iban(iban_valid[["LV"]], country = "LV")))
+  expect_true(all(check_iban(iban_valid[["LB"]], country = "LB")))
+  expect_true(all(check_iban(iban_valid[["LI"]], country = "LI")))
+  expect_true(all(check_iban(iban_valid[["LT"]], country = "LT")))
+  expect_true(all(check_iban(iban_valid[["LU"]], country = "LU")))
+  expect_true(all(check_iban(iban_valid[["MK"]], country = "MK")))
+  expect_true(all(check_iban(iban_valid[["MT"]], country = "MT")))
+  expect_true(all(check_iban(iban_valid[["MU"]], country = "MU")))
+  expect_true(all(check_iban(iban_valid[["YT"]], country = "YT")))
+  expect_true(all(check_iban(iban_valid[["MC"]], country = "MC")))
+  expect_true(all(check_iban(iban_valid[["ME"]], country = "ME")))
+  expect_true(all(check_iban(iban_valid[["NL"]], country = "NL")))
+  expect_true(all(check_iban(iban_valid[["NC"]], country = "NC")))
+  expect_true(all(check_iban(iban_valid[["NO"]], country = "NO")))
+  expect_true(all(check_iban(iban_valid[["PL"]], country = "PL")))
+  expect_true(all(check_iban(iban_valid[["PT"]], country = "PT")))
+  expect_true(all(check_iban(iban_valid[["RO"]], country = "RO")))
+  expect_true(all(check_iban(iban_valid[["PM"]], country = "PM")))
+  expect_true(all(check_iban(iban_valid[["SM"]], country = "SM")))
+  expect_true(all(check_iban(iban_valid[["SA"]], country = "SA")))
+  expect_true(all(check_iban(iban_valid[["RS"]], country = "RS")))
+  expect_true(all(check_iban(iban_valid[["SK"]], country = "SK")))
+  expect_true(all(check_iban(iban_valid[["SI"]], country = "SI")))
+  expect_true(all(check_iban(iban_valid[["ES"]], country = "ES")))
+  expect_true(all(check_iban(iban_valid[["SE"]], country = "SE")))
+  expect_true(all(check_iban(iban_valid[["CH"]], country = "CH")))
+  expect_true(all(check_iban(iban_valid[["TN"]], country = "TN")))
+  expect_true(all(check_iban(iban_valid[["TR"]], country = "TR")))
+  expect_true(all(check_iban(iban_valid[["GB"]], country = "GB")))
+  expect_true(all(check_iban(iban_valid[["WF"]], country = "WF")))
+})
+
+test_that("VIN numbers can be successfully validated", {
+  
+  expect_true(all(check_vin(vin_valid)))
+  expect_true(all(!check_vin(vin_invalid)))
+})

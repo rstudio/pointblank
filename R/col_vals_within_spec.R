916834aa448585a449845644b03bb09f650f58d6
Richard Iannone <riannone@me.com>
2021-04-19 00:46:07 -0400

    Update col_vals_within_spec.R

--- R/col_vals_within_spec.R
+++ R/col_vals_within_spec.R
@@ -23,14 +23,15 @@
 #' The `col_vals_within_spec()` validation function, the
 #' `expect_col_vals_within_spec()` expectation function, and the
 #' `test_col_vals_within_spec()` test function all check whether column values
-#' in a table correspond to a specification (`spec`) type. The validation step
-#' function can be used directly on a data table or with an *agent* object
-#' (technically, a `ptblank_agent` object) whereas the expectation and test
-#' functions can only be used with a data table. The types of data tables that
-#' can be used include data frames, tibbles, database tables (`tbl_dbi`), and
-#' Spark DataFrames (`tbl_spark`). Each validation step or expectation will
-#' operate over the number of test units that is equal to the number of rows in
-#' the table (after any `preconditions` have been applied).
+#' in a table correspond to a specification (`spec`) type (details of which are
+#' available in the *Specifications* section). The validation step function
+#' can be used directly on a data table or with an *agent* object (technically,
+#' a `ptblank_agent` object) whereas the expectation and test functions can only
+#' be used with a data table. The types of data tables that can be used include
+#' data frames, tibbles, database tables (`tbl_dbi`), and Spark DataFrames
+#' (`tbl_spark`). Each validation step or expectation will operate over the
+#' number of test units that is equal to the number of rows in the table (after
+#' any `preconditions` have been applied).
 #' 
 #' @section Specifications:
 #' A specification type must be used with the `spec` argument. This is a

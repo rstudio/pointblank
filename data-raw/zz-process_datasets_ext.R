7b0e16e1910a92ad361838087d625c957770ee49
Richard Iannone <riannone@me.com>
2021-07-18 23:46:25 -0400

    Update zz-process_datasets_ext.R

--- data-raw/zz-process_datasets_ext.R
+++ data-raw/zz-process_datasets_ext.R
@@ -2,11 +2,13 @@ library(usethis)
 
 source("data-raw/01-small_table.R")
 source("data-raw/04-specifications.R")
+source("data-raw/05-game_revenue.R")
 
 # Create external datasets
 
 usethis::use_data(
   small_table,
   specifications,
+  game_revenue,
   internal = FALSE, overwrite = TRUE
 )

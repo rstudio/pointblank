67a4eea5415c6300a1e8f89095034773e197e73a
Richard Iannone <riannone@me.com>
2021-08-22 20:52:05 -0400

    Update several `expect_*()` fns

--- R/col_is_numeric.R
+++ R/col_is_numeric.R
@@ -274,7 +274,7 @@ expect_col_is_numeric <- function(object,
     interrogate() %>%
     .$validation_set
   
-  x <- vs$notify %>% all()
+  x <- vs$notify
   
   threshold_type <- get_threshold_type(threshold = threshold)
   
@@ -282,6 +282,25 @@ expect_col_is_numeric <- function(object,
     failed_amount <- vs$f_failed
   } else {
     failed_amount <- vs$n_failed
+  }
+  
+  # If several validations were performed serially (due to supplying
+  # multiple columns)
+  if (length(x) > 1 && any(x)) {
+    
+    # Get the index (step) of the first failure instance
+    fail_idx <- which(x)[1]
+    
+    # Get the correct, single `failed_amount` for the first
+    # failure instance
+    failed_amount <- failed_amount[fail_idx]
+    
+    # Redefine `x` as a single TRUE value
+    x <- TRUE
+    
+  } else {
+    x <- any(x)
+    fail_idx <- 1
   }
   
   if (inherits(vs$capture_stack[[1]]$warning, "simpleWarning")) {
@@ -293,7 +312,7 @@ expect_col_is_numeric <- function(object,
   
   act <- testthat::quasi_label(enquo(x), arg = "object")
   
-  column_text <- prep_column_text(vs$column[[1]])
+  column_text <- prep_column_text(vs$column[[fail_idx]])
   col_type <- "numeric"
   
   testthat::expect(

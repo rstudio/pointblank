e74002911664f7e716b385c89d83e786965c9db4
Richard Iannone <riannone@me.com>
2021-11-12 11:51:25 -0500

    Update datasets.R

--- R/datasets.R
+++ R/datasets.R
@@ -49,16 +49,16 @@
 #'
 "small_table"
 
-#' A SQLite version of the `small_table` dataset
+#' An SQLite version of the `small_table` dataset
 #' 
-#' The `small_table_sqlite()` function creates a SQLite, `tbl_dbi` version of
+#' The `small_table_sqlite()` function creates an SQLite, `tbl_dbi` version of
 #' the `small_table` dataset. A requirement is the availability of the **DBI**
 #' and **RSQLite** packages. These packages can be installed by using 
 #' `install.packages("DBI")` and `install.packages("RSQLite")`.
 #' 
 #' @examples
 #' # Use `small_table_sqlite()` to
-#' # create a SQLite version of the
+#' # create an SQLite version of the
 #' # `small_table` table
 #' #
 #' # small_table_sqlite <- small_table_sqlite()

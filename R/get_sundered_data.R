977633b8f69868a33d7f18803df75019ac18e103
Richard Iannone <riannone@me.com>
2022-01-05 22:51:25 -0500

    Update get_sundered_data.R

--- R/get_sundered_data.R
+++ R/get_sundered_data.R
@@ -76,7 +76,7 @@
 #' # `interrogate()` immediately
 #' agent <-
 #'   create_agent(
-#'     read_fn = ~ small_table %>%
+#'     tbl = ~ small_table %>%
 #'       dplyr::select(a:f),
 #'     label = "`get_sundered_data()`"
 #'   ) %>%

57570833a40e6c5d9289f871d62d1641baf4c534
Richard Iannone <riannone@me.com>
2022-03-09 23:35:57 -0500

    Update col_vals_in_set.R

--- R/col_vals_in_set.R
+++ R/col_vals_in_set.R
@@ -245,16 +245,18 @@ NULL
 #' @rdname col_vals_in_set
 #' @import rlang
 #' @export
-col_vals_in_set <- function(x,
-                            columns,
-                            set,
-                            preconditions = NULL,
-                            segments = NULL,
-                            actions = NULL,
-                            step_id = NULL,
-                            label = NULL,
-                            brief = NULL,
-                            active = TRUE) {
+col_vals_in_set <- function(
+    x,
+    columns,
+    set,
+    preconditions = NULL,
+    segments = NULL,
+    actions = NULL,
+    step_id = NULL,
+    label = NULL,
+    brief = NULL,
+    active = TRUE
+) {
   
   # Get `columns` as a label
   columns_expr <- 
@@ -354,11 +356,13 @@ col_vals_in_set <- function(x,
 #' @rdname col_vals_in_set
 #' @import rlang
 #' @export
-expect_col_vals_in_set <- function(object,
-                                   columns,
-                                   set,
-                                   preconditions = NULL,
-                                   threshold = 1) {
+expect_col_vals_in_set <- function(
+    object,
+    columns,
+    set,
+    preconditions = NULL,
+    threshold = 1
+) {
   
   fn_name <- "expect_col_vals_in_set"
   
@@ -432,11 +436,13 @@ expect_col_vals_in_set <- function(object,
 #' @rdname col_vals_in_set
 #' @import rlang
 #' @export
-test_col_vals_in_set <- function(object,
-                                 columns,
-                                 set,
-                                 preconditions = NULL,
-                                 threshold = 1) {
+test_col_vals_in_set <- function(
+    object,
+    columns,
+    set,
+    preconditions = NULL,
+    threshold = 1
+) {
   
   vs <- 
     create_agent(tbl = object, label = "::QUIET::") %>%

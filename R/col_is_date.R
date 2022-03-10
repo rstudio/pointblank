2553fa9c78d9e43d19e4d9e7c8976dd50f48d742
Richard Iannone <riannone@me.com>
2022-03-09 23:35:25 -0500

    Update col_is_date.R

--- R/col_is_date.R
+++ R/col_is_date.R
@@ -174,13 +174,15 @@ NULL
 #' @rdname col_is_date
 #' @import rlang
 #' @export
-col_is_date <- function(x,
-                        columns,
-                        actions = NULL,
-                        step_id = NULL,
-                        label = NULL,
-                        brief = NULL,
-                        active = TRUE) {
+col_is_date <- function(
+    x,
+    columns,
+    actions = NULL,
+    step_id = NULL,
+    label = NULL,
+    brief = NULL,
+    active = TRUE
+) {
   
   preconditions <- NULL
   values <- NULL
@@ -257,9 +259,11 @@ col_is_date <- function(x,
 #' @rdname col_is_date
 #' @import rlang
 #' @export
-expect_col_is_date <- function(object,
-                               columns,
-                               threshold = 1) {
+expect_col_is_date <- function(
+    object,
+    columns,
+    threshold = 1
+) {
   
   fn_name <- "expect_col_is_date"
   
@@ -330,9 +334,11 @@ expect_col_is_date <- function(object,
 #' @rdname col_is_date
 #' @import rlang
 #' @export
-test_col_is_date <- function(object,
-                             columns,
-                             threshold = 1) {
+test_col_is_date <- function(
+    object,
+    columns,
+    threshold = 1
+) {
   
   vs <- 
     create_agent(tbl = object, label = "::QUIET::") %>%

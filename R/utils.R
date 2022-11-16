b497bb0af0701c306bce626b86711a6644f1d029
Pacha <mavargas11@uc.cl>
2022-11-15 19:32:56 -0500

    fix #446

--- R/utils.R
+++ R/utils.R
@@ -810,7 +810,7 @@ get_tbl_information_dbi <- function(tbl) {
     # nocov end
       
   } else {
-    tbl_src <- gsub("^([a-z]*).*", "\\1", get_tbl_dbi_src_details(tbl))
+    tbl_src <- gsub("^([a-z]*).*|", "\\1", tolower(get_tbl_dbi_src_details(tbl)))
   }
   
   db_tbl_name <- as.character(dbplyr::remote_name(tbl))

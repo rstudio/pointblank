099ba5f0ee14f0f3ee6ac18a9470b0c91868962b
June Choe <yjchoe@sas.upenn.edu>
2023-10-17 11:57:37 -0400

    namespace tidyselect fns in test

--- tests/testthat/test-tidyselect_integration.R
+++ tests/testthat/test-tidyselect_integration.R
@@ -1,6 +1,6 @@
 test_that("Full range of tidyselect features available in column selection", {
   
-  tbl <- tibble(x = 1:2, y = 1:2, yy = 1:2, nonunique = 1)
+  tbl <- data.frame(x = 1:2, y = 1:2, yy = 1:2, nonunique = 1)
   
   # Single symbol
   expect_success(expect_rows_distinct(tbl, x))
@@ -17,9 +17,9 @@ test_that("Full range of tidyselect features available in column selection", {
   expect_failure(expect_rows_distinct(tbl, c(nonunique)))
   
   # {tidyselect} functions
-  expect_success(expect_rows_distinct(tbl, all_of("x")))
-  expect_success(expect_rows_distinct(tbl, all_of(c("x", "nonunique"))))
-  expect_failure(expect_rows_distinct(tbl, all_of("nonunique")))
+  expect_success(expect_rows_distinct(tbl, tidyselect::all_of("x")))
+  expect_success(expect_rows_distinct(tbl, tidyselect::all_of(c("x", "nonunique"))))
+  expect_failure(expect_rows_distinct(tbl, tidyselect::all_of("nonunique")))
   
   # NEW: {tidyselect} integer indexing
   expect_success(expect_rows_distinct(tbl, 1))
@@ -33,10 +33,10 @@ test_that("Full range of tidyselect features available in column selection", {
   
   # NEW: {tidyselect} functions in complex expressions
   exist_col <- "y"
-  expect_success(expect_rows_distinct(tbl, c(x, all_of(exist_col))))
+  expect_success(expect_rows_distinct(tbl, c(x, tidyselect::all_of(exist_col))))
   nonexist_col <- "z"
-  expect_error(expect_rows_distinct(tbl, c(x, all_of(nonexist_col))))
-  expect_success(expect_rows_distinct(tbl, c(x, any_of(nonexist_col))))
+  expect_error(expect_rows_distinct(tbl, c(x, tidyselect::all_of(nonexist_col))))
+  expect_success(expect_rows_distinct(tbl, c(x, tidyselect::any_of(nonexist_col))))
   
   # DEPRECATION: Supplying a character vector variable still works, but signals deprecation:
   options(lifecycle_verbosity = "warning")

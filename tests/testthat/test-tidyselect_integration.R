f07e3960565c74318c4997baee1b4430761a6db0
June Choe <yjchoe@sas.upenn.edu>
2023-10-21 20:12:31 -0400

    clean up tidyselect test file

--- tests/testthat/test-tidyselect_integration.R
+++ tests/testthat/test-tidyselect_integration.R
@@ -1,6 +1,6 @@
 test_that("Full range of tidyselect features available in column selection", {
   
-  tbl <- data.frame(x = 1:2, y = 1:2, yy = 1:2, nonunique = "A")
+  tbl <- data.frame(x = 1:2, y = 1:2, nonunique = "A")
   
   # Single symbol
   expect_success(expect_rows_distinct(tbl, x))
@@ -23,13 +23,13 @@ test_that("Full range of tidyselect features available in column selection", {
   
   # NEW: {tidyselect} integer indexing
   expect_success(expect_rows_distinct(tbl, 1))
-  expect_success(expect_rows_distinct(tbl, c(1, 4)))
-  expect_failure(expect_rows_distinct(tbl, 4))
+  expect_success(expect_rows_distinct(tbl, c(1, 3)))
+  expect_failure(expect_rows_distinct(tbl, 3))
   
   # NEW: {tidyselect} negative indexing
-  expect_success(expect_rows_distinct(tbl, -(2:4)))
   expect_success(expect_rows_distinct(tbl, -(2:3)))
-  expect_failure(expect_rows_distinct(tbl, -(1:3)))
+  expect_success(expect_rows_distinct(tbl, -2))
+  expect_failure(expect_rows_distinct(tbl, -(1:2)))
   
   # NEW: {tidyselect} `where()` predicate:
   expect_success(expect_rows_distinct(tbl, !tidyselect::where(is.character)))
@@ -43,16 +43,50 @@ test_that("Full range of tidyselect features available in column selection", {
   expect_error(expect_rows_distinct(tbl, c(x, tidyselect::all_of(nonexist_col))))
   expect_success(expect_rows_distinct(tbl, c(x, tidyselect::any_of(nonexist_col))))
   
-  # DEPRECATION: Supplying a character vector variable still works, but signals deprecation:
-  options(lifecycle_verbosity = "warning")
+  # Supplying a character vector variable still works, but signals deprecation:
+  rlang::local_options(lifecycle_verbosity = "warning")
   expect_success(expect_warning(
     expect_rows_distinct(tbl, exist_col),
     "Using an external vector in selections was deprecated in tidyselect 1.1.0."
   ))
   
-  # For `rows_distinct()` specifically, NULL = "select everything" behavior implemented in tidyselect:
-  expect_success(expect_rows_distinct(tbl, ))
-  tbl_only_nonunique <- tbl[,"nonunique", drop = FALSE]
-  expect_failure(expect_rows_distinct(tbl_only_nonunique, ))
+})
+
+
+test_that("'NULL = select everything' behavior in rows_*() validation functions", {
+
+  # For `rows_*()` functions specifically, empty/NULL = "select everything" behavior:
+  expect_success(expect_rows_distinct(data.frame(x = 1, y = 2)))
+  expect_success(expect_rows_complete(data.frame(x = 1, y = 2)))
+  expect_failure(expect_rows_distinct(data.frame(x = c(1, 1))))
+  expect_failure(expect_rows_complete(data.frame(x = c(1, NA))))
+  expect_success(expect_rows_distinct(data.frame(x = 1, y = 2), columns = NULL))
+  expect_success(expect_rows_complete(data.frame(x = 1, y = 2), columns = NULL))
+  expect_failure(expect_rows_distinct(data.frame(x = c(1, 1)), columns = NULL))
+  expect_failure(expect_rows_complete(data.frame(x = c(1, NA)), columns = NULL))
   
+  # Report shows all column names with empty `columns` argument
+  expect_equal({
+    small_table %>%
+      create_agent() %>% 
+      rows_distinct() %>% 
+      rows_complete() %>% 
+      interrogate() %>% 
+      {.$validation_set$column} %>% 
+      unlist() %>% 
+      unique()
+  }, toString(colnames(small_table)))
+  
+  # Report shows all column names with explicit NULL `columns` argument
+  expect_equal({
+    small_table %>%
+      create_agent() %>% 
+      rows_distinct(columns = NULL) %>% 
+      rows_complete(columns = NULL) %>% 
+      interrogate() %>% 
+      {.$validation_set$column} %>% 
+      unlist() %>% 
+      unique()
+  }, toString(colnames(small_table)))
+    
 })

44d510ec3892b62c92814116b86231be7b74b8da
June Choe <yjchoe@sas.upenn.edu>
2023-10-29 20:29:13 -0400

    test columns c()-expr roundtrip

--- tests/testthat/test-tidyselect_integration.R
+++ tests/testthat/test-tidyselect_integration.R
@@ -241,3 +241,30 @@ test_that("c()-expr works for serially", {
   )
   
 })
+
+test_that("explicit c()-expr make the yaml roundtrip", {
+  
+  agent_pre <- create_agent(~ small_table) %>% 
+    col_vals_lt(
+      columns = c(a, c),
+      value = 8
+    )
+  
+  agent_yaml <- tempfile()
+  yaml_write(agent_pre, expanded = FALSE, filename = agent_yaml)
+  # Writes to c()-expr
+  expect_true(any(grepl("columns: c(a, c)", readLines(agent_yaml), fixed = TRUE)))
+  
+  agent_post <- yaml_read_agent(agent_yaml)
+  # yaml_agent_string(agent_post, expanded = FALSE)
+  
+  expect_identical(
+    as_agent_yaml_list(agent_pre, expanded = FALSE),
+    as_agent_yaml_list(agent_post, expanded = FALSE)
+  )
+  expect_identical(
+    agent_pre %>% interrogate() %>% get_agent_report(display_table = FALSE),
+    agent_post %>% interrogate() %>% get_agent_report(display_table = FALSE)
+  )
+  
+})

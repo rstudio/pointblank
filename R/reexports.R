3006f798519da3ba0e3739aad1f309f7e8fff87b
June Choe <yjchoe@sas.upenn.edu>
2023-11-02 19:10:03 -0400

    since columns is always enquo-ed, no need to re-export helpers

--- R/reexports.R
+++ R/reexports.R
@@ -35,26 +35,6 @@ dplyr::between
 #' @export
 rlang::expr
 
-#' @importFrom tidyselect starts_with
-#' @export
-tidyselect::starts_with
-
-#' @importFrom tidyselect ends_with
-#' @export
-tidyselect::ends_with
-
-#' @importFrom tidyselect contains
-#' @export
-tidyselect::contains
-
-#' @importFrom tidyselect matches
-#' @export
-tidyselect::matches
-
-#' @importFrom tidyselect everything
-#' @export
-tidyselect::everything
-
 #' @importFrom blastula creds
 #' @export
 blastula::creds

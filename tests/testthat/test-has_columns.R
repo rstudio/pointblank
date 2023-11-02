be13a3e038ce8eeccfed3d9903cc1217d7171d7e
June Choe <yjchoe@sas.upenn.edu>
2023-11-02 15:34:11 -0400

    edit tests

--- tests/testthat/test-has_columns.R
+++ tests/testthat/test-has_columns.R
@@ -10,16 +10,19 @@ test_that("the `has_columns()` function works when used directly with data", {
   expect_false(small_table %>% has_columns(vars(a, h)))
   expect_false(small_table %>% has_columns(vars(h, j)))
   
-  # Select helper functions don't work with `has_columns()` and will
-  # always throw errors
-  expect_error(small_table %>% has_columns(pointblank::contains("te")))
-  expect_error(small_table %>% has_columns(pointblank::starts_with("date")))
-  expect_error(small_table %>% has_columns(pointblank::ends_with("time")))
-  expect_error(small_table %>% has_columns(pointblank::everything()))
-  expect_error(small_table %>% has_columns(pointblank::matches(".*te")))
-  
   # Expect that using inputs that are not tabular result in errors
   expect_error(has_columns(list(a = "2"), "a"))
   expect_error(has_columns(c(a = 1, b = 2), "a"))
   expect_error(has_columns(matrix(c(1, 2, 3, 4), nrow = 2), "b"))
+})
+
+test_that("the `has_columns()` function works with tidyselect", {
+  
+  # Select helpers work
+  expect_true(small_table %>% has_columns(contains("te")))
+  expect_true(small_table %>% has_columns(starts_with("date")))
+  expect_true(small_table %>% has_columns(ends_with("time")))
+  expect_true(small_table %>% has_columns(everything()))
+  expect_true(small_table %>% has_columns(matches(".*te")))
+  
 })

b91cb72787789b05b53774648a285e383ad1fed8
yjunechoe <jchoe001@gmail.com>
2024-08-19 10:12:37 -0400

    add tests

---
+++ tests/testthat/test-brief.R
@@ -0,0 +1,29 @@
+test_that("`brief` recycles when possible", {
+
+  expect_equal(
+    create_agent(small_table) |> 
+      col_exists(c("a", "b"), brief = "one") |> 
+      el("validation_set") |> 
+      el("brief"),
+    c("one", "one")
+  )
+
+  expect_equal(
+    create_agent(small_table) |> 
+      col_exists(c("a", "b"), brief = c("one", "two")) |> 
+      el("validation_set") |> 
+      el("brief"),
+    c("one", "two")
+  )
+
+  expect_error(
+    create_agent(small_table) |> 
+      col_vals_equal(
+        c("a", "b"), 0,
+        segments = vars(f),
+        brief = c("one", "two")
+    ),
+    "must be length 1 or 6, not 2"
+  )
+
+})

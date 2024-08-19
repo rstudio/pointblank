27bf33fc3ef1dbc189af75f482afc46eae0dd767
yjunechoe <jchoe001@gmail.com>
2024-08-19 10:28:38 -0400

    use magrittr pipe

--- tests/testthat/test-brief.R
+++ tests/testthat/test-brief.R
@@ -1,23 +1,23 @@
 test_that("`brief` recycles when possible", {
 
   expect_equal(
-    create_agent(small_table) |> 
-      col_exists(c("a", "b"), brief = "one") |> 
-      el("validation_set") |> 
+    create_agent(small_table) %>% 
+      col_exists(c("a", "b"), brief = "one") %>% 
+      el("validation_set") %>% 
       el("brief"),
     c("one", "one")
   )
 
   expect_equal(
-    create_agent(small_table) |> 
-      col_exists(c("a", "b"), brief = c("one", "two")) |> 
-      el("validation_set") |> 
+    create_agent(small_table) %>% 
+      col_exists(c("a", "b"), brief = c("one", "two")) %>% 
+      el("validation_set") %>% 
       el("brief"),
     c("one", "two")
   )
 
   expect_error(
-    create_agent(small_table) |> 
+    create_agent(small_table) %>% 
       col_vals_equal(
         c("a", "b"), 0,
         segments = vars(f),

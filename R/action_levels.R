06d943ff9480d70d6d665cfd6050e870be8c9d17
yjunechoe <jchoe001@gmail.com>
2025-03-25 14:35:31 -0400

    standardize fns to action_fns object internally

--- R/action_levels.R
+++ R/action_levels.R
@@ -333,7 +333,9 @@ action_levels <- function(
   error <- error %||% stop_at
   critical <- critical %||% notify_at
 
-  fns <- normalize_fns_list(fns = fns)
+  if (!inherits(fns, "action_fns")) {
+    fns <- normalize_fns_list(fns = fns)
+  }
 
   warn_list <- normalize_fraction_count(warn)
   error_list <- normalize_fraction_count(error)
@@ -385,41 +387,44 @@ stop_on_fail <- function(stop_at = 1) {
   error_on_fail(error = stop_at)
 }
 
+action_fns <- function(warn = NULL, error = NULL, critical = NULL) {
+  fns <- structure(
+    list(warn = warn, error = error, critical = critical,
+         stop = error, notify = critical), # temp compatibility with rest of internals
+    class = "action_fns"
+  )
+  if (!all(vapply(fns, function(x) is.null(x) || is_formula(x), logical(1)))) {
+    cli::cli_abort("Action functions must be supplied as formulas.")
+  }
+  fns
+}
+
 normalize_fns_list <- function(fns) {
 
-  if (is.null(fns) || (is.list(fns) && length(fns) == 0)) {
-    return(list(warn = NULL, stop = NULL, notify = NULL))
+  if (is.null(fns)) {
+    return(action_fns())
   }
 
-  are_formulas <-
-    fns %>%
-    vapply(
-      FUN.VALUE = logical(1), USE.NAMES = FALSE,
-      FUN = function(x) rlang::is_formula(x)
-    )
+  nms <- c("warn", "error", "critical",
+                   "stop",  "notify")
 
-  if (!all(are_formulas)) {
+  stopifnot(
+    "`fns` must be a `action_fns()` object" = is.list(fns),
+    "The `fns` list must be fully named." = !("" %in% rlang::names2(fns)),
+    "`fns` must contain valid action levels" = all(names(fns) %in% nms)
+  )
 
-    stop(
-      "All components of the `fns` list must be formulas.",
-      call. = FALSE
-    )
-  }
+  cli::cli_warn(c(
+    "!" = "Passing a list to `fns` is deprecated.",
+    " " = "Please use `action_fns()` instead."
+  ))
 
-  if ("" %in% names(fns)) {
+  warn <- fns$warn %||% fns$warn_at
+  error <- fns$error %||% fns$error_at
+  critical <- fns$critical %||% fns$notify_at
 
-    stop("The `fns` list must be fully named.", call. = FALSE)
-  }
+  action_fns(warn, error, critical)
 
-  if (!all(names(fns) %in% c("warn", "stop", "notify"))) {
-
-    stop(
-      "All names in the `fns` list must be one of `warn`, `stop`, or `notify`.",
-      call. = FALSE
-    )
-  }
-
-  fns
 }
 
 normalize_fraction_count <- function(x) {

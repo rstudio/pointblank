4635084e0620521aff0b9cf536977ed319bb6fb0
Richard Iannone <riannone@me.com>
2026-08-20 22:56:20 -0400

    Update yaml_read_agent.R

--- R/yaml_read_agent.R
+++ R/yaml_read_agent.R
@@ -311,11 +311,11 @@ yaml_agent_interrogate <- function(
 #'   col_exists(columns = c(date, date_time)) |>
 #'   col_vals_regex(
 #'     columns = b,
-#'     regex = "[0-9]-[a-z]{3}-[0-9]{3}"
+#'     pattern = "[0-9]-[a-z]{3}-[0-9]{3}"
 #'   ) |>
 #'   rows_distinct() |>
 #'   col_vals_gt(columns = d, value = 100) |>
-#'   col_vals_lte(columns = c, value = 5)
+#'   col_vals_le(columns = c, value = 5)
 #' ```
 #'
 #' The agent can be written to a **pointblank** YAML file with [yaml_write()].
@@ -365,14 +365,14 @@ yaml_agent_interrogate <- function(
 #'   ) |>
 #'   col_vals_regex(
 #'     columns = b,
-#'     regex = "[0-9]-[a-z]{3}-[0-9]{3}"
+#'     pattern = "[0-9]-[a-z]{3}-[0-9]{3}"
 #'   ) |>
 #'   rows_distinct() |>
 #'   col_vals_gt(
 #'     columns = d,
 #'     value = 100
 #'   ) |>
-#'   col_vals_lte(
+#'   col_vals_le(
 #'     columns = c,
 #'     value = 5
 #'   )

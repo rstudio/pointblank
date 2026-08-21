2b31205a64f0c1f3bf37c6f77bcecfbfbf159818
Richard Iannone <riannone@me.com>
2026-08-20 22:55:59 -0400

    Update conjointly.R

--- R/conjointly.R
+++ R/conjointly.R
@@ -54,7 +54,7 @@
 #'   A collection one-sided formulas that consist of validation functions that
 #'   validate row units (the `col_vals_*()` series), column existence
 #'   ([col_exists()]), or column type (the `col_is_*()` series). An example of
-#'   this is `~ col_vals_gte(., a, 5.5), ~ col_vals_not_null(., b`).
+#'   this is `~ col_vals_ge(., a, 5.5), ~ col_vals_not_null(., b`).
 #'
 #' @param .list *Alternative to `...`*
 #'
